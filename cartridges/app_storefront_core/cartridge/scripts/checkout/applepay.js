730ae07342b46bb4ec7cc4a217ca161e95c8fbce
Joe Greenwood <grnwood@gmail.com>
2017-02-07 07:52:48 -0600

    latest

--- cartridges/app_storefront_core/cartridge/scripts/checkout/applepay.js
+++ cartridges/app_storefront_core/cartridge/scripts/checkout/applepay.js
@@ -1,4 +1,5 @@
 var Status = require('dw/system/Status');
+var ApplePayHookResult = require('dw/extensions/applepay/ApplePayHookResult');
 var PaymentInstrument = require('dw/order/PaymentInstrument');
 var Logger = require('dw/system/Logger');
 
@@ -17,4 +18,13 @@ Payment Provider for authorization.');
     var paymentTransaction = paymentInstrument.getPaymentTransaction();
     paymentTransaction.setTransactionID('DUMMY-APPLEPAY-PSP-TRANSACTION-ID');
     return new Status(Status.OK);
+};
+
+exports.getRequest = function (basket, request) {
+    if (request.shippingContact) {
+        // convert country code from lower case to upper case
+        request.shippingContact.countryCode =
+            request.shippingContact.countryCode.toUpperCase();
+    }
+    return new ApplePayHookResult(new Status(Status.OK), null);
 };
