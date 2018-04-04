6416c4993a479faaaccfdea8fa6fa37bb8699490
basm <bas.maassen@adyen.com>
2018-04-04 15:42:30 +0200

    added MFRA cartridge

---
+++ cartridges/app_adyen_MFRA/cartridge/scripts/hooks/payment/processor/adyen_credit.js
@@ -0,0 +1,12 @@
+'use strict';
+require('*/cartridge/controllers/CheckoutServices.js');
+
+function Handle(args) {
+
+    alert('hello'); // my test
+
+    return {success: true};
+
+}
+
+exports.Handle = Handle;
