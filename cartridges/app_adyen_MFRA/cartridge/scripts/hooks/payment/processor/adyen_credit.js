1dc27fceeb08578066a3fa24150e12399e4ddf22
basm <bas.maassen@adyen.com>
2018-04-11 10:21:30 +0200

    Added hooks, template credit card

--- cartridges/app_adyen_MFRA/cartridge/scripts/hooks/payment/processor/adyen_credit.js
+++ cartridges/app_adyen_MFRA/cartridge/scripts/hooks/payment/processor/adyen_credit.js
@@ -1,12 +1,17 @@
+/**
+ * 
+ */
+
+
 'use strict';
-require('*/cartridge/controllers/CheckoutServices.js');
-
-function Handle(args) {
-
-    alert('hello'); // my test
-
+var server = require('server');
+//var Cart = require("app_storefront_base/cartridge/controllers/Cart");
+function Handle(currentBasket, paymentInformation) {
+	
+	var cart = require("app_storefront_base/cartridge/model/cart");
+	var cartModel = cart.CartModel(currentBasket);
     return {success: true};
-
+ 
 }
 
 exports.Handle = Handle;
