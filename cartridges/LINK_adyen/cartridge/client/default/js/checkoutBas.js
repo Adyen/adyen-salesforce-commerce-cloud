5f586eba9ce1eae10c9f4e0b51f2ff6965889c0e
basm <bas.maassen@adyen.com>
2018-05-31 15:37:19 +0200

    Credit card payments (without Save card details, 3D Secure)
    diff --git a/cartridges/LINK_adyen/cartridge/client/default/js/checkout/billing.js b/cartridges/LINK_adyen/cartridge/client/default/js/checkout/billing.js
    new file mode 100644
    index 0000000..2df94e9
    --- /dev/null
    +++ b/cartridges/LINK_adyen/cartridge/client/default/js/checkout/billing.js
    @@ -0,0 +1,8 @@
    +//module.exports = {
    +    // clearCreditCardForm : function() {
    +    //     alert('cleared');
    +    // }
    +//};
    +
    +
    +
    diff --git a/cartridges/LINK_adyen/cartridge/client/default/js/checkout/checkout.js b/cartridges/LINK_adyen/cartridge/client/default/js/checkout/checkout.js
    new file mode 100644
    index 0000000..f8d5095
    --- /dev/null
    +++ b/cartridges/LINK_adyen/cartridge/client/default/js/checkout/checkout.js
    @@ -0,0 +1,6 @@
    +// module.exports = {
    +//     initialize : function() {
    +//         //alert('cleared');
    +//         //console.log('init');
    +//     }
    +// };
    diff --git a/cartridges/LINK_adyen/cartridge/client/default/js/checkoutBas.js b/cartridges/LINK_adyen/cartridge/client/default/js/checkoutBas.js
    new file mode 100644
    index 0000000..a731ca3
    --- /dev/null
    +++ b/cartridges/LINK_adyen/cartridge/client/default/js/checkoutBas.js
    @@ -0,0 +1,10 @@
    +// 'use strict';
    +//
    +// var processInclude = require('./base/util');
    +//
    +// $(document).ready(function() {
    +//     // var test = "bastest";
    +//     // console.log('checkout');
    +//     // processInclude(require('./checkout/checkout'));
    +//     // processInclude(require('./base/checkout/checkout'));
    +// });
    diff --git a/cartridges/LINK_adyen/cartridge/scripts/hooks.json b/cartridges/LINK_adyen/cartridge/scripts/hooks.json
    index 794c1ae..5a61448 100644
    --- a/cartridges/LINK_adyen/cartridge/scripts/hooks.json
    +++ b/cartridges/LINK_adyen/cartridge/scripts/hooks.json
    @@ -3,10 +3,6 @@
     		{
     			"name": "app.payment.processor.adyen_credit",
     			"script": "./hooks/payment/processor/adyen_credit"
    -		},
    -		{
    -			"name": "app.payment.processor.adyen",
    -			"script": "./hooks/payment/processor/adyen"
     		}
     	]
     }
    diff --git a/cartridges/app_adyen_MFRA/cartridge/scripts/hooks.json b/cartridges/app_adyen_MFRA/cartridge/scripts/hooks.json
    deleted file mode 100644
    index 5a61448..0000000
    --- a/cartridges/app_adyen_MFRA/cartridge/scripts/hooks.json
    +++ /dev/null
    @@ -1,8 +0,0 @@
    -{
    -	"hooks": [
    -		{
    -			"name": "app.payment.processor.adyen_credit",
    -			"script": "./hooks/payment/processor/adyen_credit"
    -		}
    -	]
    -}
    diff --git a/cartridges/app_adyen_MFRA/cartridge/scripts/hooks/payment/processor/adyen_credit.js b/cartridges/app_adyen_MFRA/cartridge/scripts/hooks/payment/processor/adyen_credit.js
    deleted file mode 100644
    index aa4d2dc..0000000
    --- a/cartridges/app_adyen_MFRA/cartridge/scripts/hooks/payment/processor/adyen_credit.js
    +++ /dev/null
    @@ -1,17 +0,0 @@
    -/**
    - *
    - */
    -
    -
    -'use strict';
    -var server = require('server');
    -//var Cart = require("app_storefront_base/cartridge/controllers/Cart");
    -function Handle(currentBasket, paymentInformation) {
    -
    -	var cart = require("app_storefront_base/cartridge/model/cart");
    -	var cartModel = cart.CartModel(currentBasket);
    -    return {success: true};
    -
    -}
    -
    -exports.Handle = Handle;
    \ No newline at end of file
    diff --git a/cartridges/app_adyen_MFRA/package.json b/cartridges/app_adyen_MFRA/package.json
    deleted file mode 100644
    index e835f22..0000000
    --- a/cartridges/app_adyen_MFRA/package.json
    +++ /dev/null
    @@ -1,6 +0,0 @@
    -{
    -   "hooks": "./cartridge/scripts/hooks.json",
    -   "paths": {
    -   "base": "../sitegenesis-mfra/cartridges/app_storefront_base/"
    -   }
    -}

---
+++ cartridges/LINK_adyen/cartridge/client/default/js/checkoutBas.js
@@ -0,0 +1,10 @@
+// 'use strict';
+//
+// var processInclude = require('./base/util');
+//
+// $(document).ready(function() {
+//     // var test = "bastest";
+//     // console.log('checkout');
+//     // processInclude(require('./checkout/checkout'));
+//     // processInclude(require('./base/checkout/checkout'));
+// });
