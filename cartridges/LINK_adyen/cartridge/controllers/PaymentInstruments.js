d483f11de5a1e801ea68b0d5d16fee81bea24b2c
basm <bas.maassen@adyen.com>
2018-09-04 14:10:49 +0200

    Show error message on Add Payment

--- cartridges/LINK_adyen/cartridge/controllers/PaymentInstruments.js
+++ cartridges/LINK_adyen/cartridge/controllers/PaymentInstruments.js
@@ -7,7 +7,6 @@ var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var AdyenHelper = require ("int_adyen/cartridge/scripts/util/AdyenHelper");
-var Logger = require('dw/system/Logger');
 
 server.prepend('List', userLoggedIn.validateLoggedIn, consentTracking.consent, function (req, res, next) {
     require('int_adyen/cartridge/scripts/UpdateSavedCards').updateSavedCards({CurrentCustomer : req.currentCustomer.raw});
@@ -19,36 +18,17 @@ function getEncryptedData(){
     return paymentForm.adyenEncryptedData.value;
 }
 
-/**
- * Creates an object from form values
- * @param {Object} paymentForm - form object
- * @returns {Object} a plain object of payment instrument
- */
-function getDetailsObject(paymentForm) {
-    return {
-        name: paymentForm.cardOwner.value,
-        cardNumber: paymentForm.cardNumber.value,
-        cardType: paymentForm.cardType.value,
-        expirationMonth: paymentForm.expirationMonth.value,
-        expirationYear: paymentForm.expirationYear.value,
-        paymentForm: paymentForm
-    };
-}
-
 server.replace('SavePayment', csrfProtection.validateAjaxRequest, function (req, res, next) {
-    var formErrors = require('*/cartridge/scripts/formErrors');
     var viewData = res.getViewData();
     viewData.adyenEncryptedData =  getEncryptedData();
     res.setViewData(viewData);
 
     var paymentForm = server.forms.getForm('creditCard');
-    var result = getDetailsObject(paymentForm);
-
     if (paymentForm.valid) {
-        res.setViewData(result);
-        this.on('route:BeforeComplete', function (req, res) { // eslint-disable-line no-shadow
+        this.on('route:BeforeComplete', function (req, res) {
             var URLUtils = require('dw/web/URLUtils');
             var CustomerMgr = require('dw/customer/CustomerMgr');
+            var Resource = require('dw/web/Resource');
 
             var customer = CustomerMgr.getCustomerByCustomerNumber(
                 req.currentCustomer.profile.customerNo
@@ -61,19 +41,20 @@ server.replace('SavePayment', csrfProtection.validateAjaxRequest, function (req,
             if (createRecurringPaymentAccountResult.error) {
                 res.json({
                     success: false,
-                    fields: formErrors.getFormErrors(paymentForm)
+                    error: [Resource.msg('error.card.information.error', 'creditCard', null)]
                 });
             }
-
-            res.json({
-                success: true,
-                redirectUrl: URLUtils.url('PaymentInstruments-List').toString()
-            });
+            else{
+                res.json({
+                    success: true,
+                    redirectUrl: URLUtils.url('PaymentInstruments-List').toString()
+                });
+            }
         });
     } else {
         res.json({
             success: false,
-            fields: formErrors.getFormErrors(paymentForm)
+            error: [Resource.msg('error.card.information.error', 'creditCard', null)]
         });
     }
     return next();
@@ -83,13 +64,12 @@ server.append('DeletePayment', userLoggedIn.validateLoggedInAjax, function (req,
     var CustomerMgr = require('dw/customer/CustomerMgr');
     var payment = res.getViewData();
 
-    if(!empty(payment)){
+    if(!empty(payment)) {
         var customer = CustomerMgr.getCustomerByCustomerNumber(
             req.currentCustomer.profile.customerNo
         );
         var tokenToDelete = AdyenHelper.getCardToken(payment.UUID, customer);
-        Logger.getLogger("Adyen").error("tokenToDelete = " + tokenToDelete);
-        if  (!empty(tokenToDelete)) {
+        if (!empty(tokenToDelete)) {
             var result = require('int_adyen/cartridge/scripts/adyenDeleteRecurringPayment').deleteRecurringPayment({
                 Customer: customer,
                 RecurringDetailReference: tokenToDelete
