497caf43f1879e849bda78b06eec39ff0f5f705e
basm <bas.maassen@adyen.com>
2018-11-13 13:32:50 +0100

    Validate credit card details, show description in DL, show selected Adyen payment method in Summary/Confirmation page

---
+++ cartridges/LINK_adyen/cartridge/models/payment.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var collections = require('*/cartridge/scripts/util/collections');
+var base = module.superModule;
+
+/**
+ * Creates an array of objects containing selected payment information
+ * @param {dw.util.ArrayList<dw.order.PaymentInstrument>} selectedPaymentInstruments - ArrayList
+ *      of payment instruments that the user is using to pay for the current basket
+ * @returns {Array} Array of objects that contain information about the selected payment instruments
+ */
+function getSelectedPaymentInstruments(selectedPaymentInstruments) {
+    return collections.map(selectedPaymentInstruments, function (paymentInstrument) {
+        var results = {
+            paymentMethod: paymentInstrument.paymentMethod,
+            amount: paymentInstrument.paymentTransaction.amount.value
+
+        };
+        results.selectedAdyenPM = !empty(paymentInstrument.custom.adyenPaymentMethod) ? paymentInstrument.custom.adyenPaymentMethod : null;
+        results.selectedIssuerName = !empty(paymentInstrument.custom.adyenIssuerName) ? paymentInstrument.custom.adyenIssuerName : null;
+
+        if (paymentInstrument.paymentMethod === 'CREDIT_CARD') {
+            results.lastFour = paymentInstrument.creditCardNumberLastDigits;
+            results.owner = paymentInstrument.creditCardHolder;
+            results.expirationYear = paymentInstrument.creditCardExpirationYear;
+            results.type = paymentInstrument.creditCardType;
+            results.maskedCreditCardNumber = paymentInstrument.maskedCreditCardNumber;
+            results.expirationMonth = paymentInstrument.creditCardExpirationMonth;
+        } else if (paymentInstrument.paymentMethod === 'GIFT_CERTIFICATE') {
+            results.giftCertificateCode = paymentInstrument.giftCertificateCode;
+            results.maskedGiftCertificateCode = paymentInstrument.maskedGiftCertificateCode;
+        }
+
+        return results;
+    });
+}
+
+/**
+ * Payment class that represents payment information for the current basket
+ * @param {dw.order.Basket} currentBasket - the target Basket object
+ * @param {dw.customer.Customer} currentCustomer - the associated Customer object
+ * @param {string} countryCode - the associated Site countryCode
+ * @constructor
+ */
+function Payment(currentBasket, currentCustomer, countryCode) {
+    base.call(this, currentBasket, currentCustomer, countryCode);
+    var paymentInstruments = currentBasket.paymentInstruments;
+
+    this.selectedPaymentInstruments = paymentInstruments ?
+        getSelectedPaymentInstruments(paymentInstruments) : null;
+}
+
+module.exports = Payment;
