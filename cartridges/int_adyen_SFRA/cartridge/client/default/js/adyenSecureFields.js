ab2f4e1ea8d85918ab012fda6c1c24a48fc3adb9
Bas Maassen <maassenbas@hotmail.com>
2018-11-26 16:20:16 +0100

    PW-673 Implement Secured Fields part2 (#92)
    * Authorisation credit card
    
    * PaymentMethod object in payment request
    
    * 3DS in Secure Fields
    
    * renamed cartridges and properties, added project files
    
    * Card validation, description, selected PM (#89)
    
    * Validate credit card details, show description in DL, show selected Adyen payment method in Summary/Confirmation page
    
    * Added content slot
    
    * directoryLookup variable
    
    * renamed cartridges and properties, added project files
    
    * Clear validations
    
    * renamed cartridges and properties, added project files
    
    * Validate credit card details, show description in DL, show selected Adyen payment method in Summary/Confirmation page
    
    * Validate credit card details, show description in DL, show selected Adyen payment method in Summary/Confirmation page
    
    * Added content slot
    
    * directoryLookup variable
    
    * Clear validations
    
    * renamed cartridges and properties, added project files
    
    * deleted LINK_adyen
    
    * Delete loggers
    
    * Renamed cartridges (#93)
    
    * Secured Fields in checkout (#87)
    
    * Secured Fields in checkout
    
    * retrieve loadContext, show error message for loading component, added locale
    
    * v1 for /OriginKeys
    
    * Deleted LINK_adyen folder
    
    * Authorisation credit card
    
    * PaymentMethod object in payment request
    
    * 3DS in Secure Fields
    
    * Delete loggers
    
    * Deleted LINK_adyen
    
    * changed path getOriginKeys
    
    * Secured Fields isValid

--- cartridges/int_adyen_SFRA/cartridge/client/default/js/adyenSecureFields.js
+++ cartridges/int_adyen_SFRA/cartridge/client/default/js/adyenSecureFields.js
@@ -5,7 +5,8 @@
 
     const checkout = new AdyenCheckout(configuration);
     const cardNode = document.getElementById('card');
-
+    var card;
+    var isValid = false;
     getConfigurationSecureFields();
 
     var originKey = "";
@@ -17,25 +18,25 @@
     };
 
     function renderCardComponent() {
-        const card = checkout.create('card', {
+        card = checkout.create('card', {
             // Mandatory fields
             originKey: originKey,
             loadingContext: loadingContext, // The environment where we should loads the secured fields from
             type: 'card',
 
             // Events
-            onChange: function() {}, // Gets triggered whenever a user changes input
-            onValid : function() {}, // Gets triggered when all fields are valid.
-            onLoad: function() {}, // Called once all the secured fields have been created (but are not yet ready to use)
-            onConfigSuccess: function() {}, // Called once the secured fields are ready to use
-            onFieldValid : function() {}, // Called as a specific secured field is validated and encrypted.
-            onBrand: function() {}, // Called once we detect the card brand
-            onError: function() {}, // Called in the case of invalid card number / invalid expiry date / incomplete field.
-            onFocus: function() {}, // Called when a secured field gains or loses focus
-            onBinValue: function(bin) {} // Provides the BIN Number of the card (up to 6 digits), called as the user types in the PAN
+            onChange: function(state) {
+                isValid = state.isValid;
+            }, // Gets triggered whenever a user changes input
+            onBrand: function(brandObject) {
+                $('#cardType').val(brandObject.brand);
+            }, // Called once we detect the card brand
+            onBinValue: function(bin) {
+                $('#cardNumber').val(bin.binValue);
+            } // Provides the BIN Number of the card (up to 6 digits), called as the user types in the PAN
         });
         card.mount(cardNode);
-    }
+    };
 
     function getConfigurationSecureFields() {
         $.ajax({
@@ -53,5 +54,22 @@
                 }
             }
         });
-    }
+    };
+
+
+    $('button[value="submit-payment"]').on('click', function (e) {
+        if($('#selectedPaymentOption').val() == 'CREDIT_CARD') {
+            if(!isValid){
+                return false;
+            }
+            else {
+                $('#adyenEncryptedCardNumber').val(card.paymentData.encryptedCardNumber);
+                $('#adyenEncryptedExpiryMonth').val(card.paymentData.encryptedExpiryMonth);
+                $('#adyenEncryptedExpiryYear').val(card.paymentData.encryptedExpiryYear);
+                $('#adyenEncryptedSecurityCode').val(card.paymentData.encryptedSecurityCode);
+            }
+        }
+    });
+
+
 
