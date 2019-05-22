1b1e5bb4c44b19434915af6c935fa9aa382c6a0c
Bas Maassen <bas.maassen@adyen.com>
2019-05-22 13:40:03 -0700

    3DS2 Initiate payment request (#153)
    * 3DS2 Initiate payment request

--- cartridges/int_adyen_SFRA/cartridge/client/default/js/adyenCheckout.js
+++ cartridges/int_adyen_SFRA/cartridge/client/default/js/adyenCheckout.js
@@ -1,3 +1,5 @@
+var threeDS2utils = require('./threeds2-js-utils.js');
+
 const configuration = {
     locale: $('#currentLocale').val(), // Defaults to en_US
     originKey: originKey,
@@ -99,6 +101,11 @@ function getConfigurationSecureFields() {
             }
         }
     });
+};
+
+function setBrowserData() {
+    var browserData = threeDS2utils.getBrowserInfo();
+    $('#browserInfo').val(JSON.stringify(browserData));
 };
 
 $('.payment-summary .edit-button').on('click', function (e) {
@@ -426,6 +433,7 @@ function setPaymentData() {
     $('#adyenEncryptedExpiryYear').val(card.paymentData.encryptedExpiryYear);
     $('#adyenEncryptedSecurityCode').val(card.paymentData.encryptedSecurityCode);
     $('#cardOwner').val(card.paymentData.holderName);
+    setBrowserData();
 }
 
 module.exports = {
