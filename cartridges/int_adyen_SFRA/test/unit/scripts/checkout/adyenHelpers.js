1284bb9e9a76235acecb45a522aa85ad31d4b565
basm <bas.maassen@adyen.com>
2019-08-16 16:54:19 +0200

    Unit tests

---
+++ cartridges/int_adyen_SFRA/test/unit/scripts/checkout/adyenHelpers.js
@@ -0,0 +1,22 @@
+var assert = require('chai').assert;
+
+var adyenHelpers = require('../../../mocks/helpers/adyenHelpers');
+
+describe('adyenHelpers', function () {
+    describe('validatePayment', function () {
+        it('should return an invalid payment', function () {
+            var req = {
+                geolocation: {
+                    countryCode: "NL"
+                },
+                currentCustomer: {
+                    raw: {}
+                }
+            };
+            var basketMgr = require('../../../mocks/dw/order/BasketMgr');
+            var currentBasket = basketMgr.getCurrentBasket();
+            var result = adyenHelpers.validatePayment(req, currentBasket);
+            assert.isTrue(result.error);
+        });
+    })
+});
