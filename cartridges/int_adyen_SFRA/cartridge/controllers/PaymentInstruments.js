1407ced33997fb48e1718542021bcf3a010cbac9
basm <bas.maassen@adyen.com>
2019-09-03 13:39:54 +0200

    WIP delete Add Payment

--- cartridges/int_adyen_SFRA/cartridge/controllers/PaymentInstruments.js
+++ cartridges/int_adyen_SFRA/cartridge/controllers/PaymentInstruments.js
@@ -14,51 +14,51 @@ server.prepend('List', userLoggedIn.validateLoggedIn, consentTracking.consent, f
     next();
 });
 
-server.replace('SavePayment', csrfProtection.validateAjaxRequest, function (req, res, next) {
-    var viewData = res.getViewData();
-    var paymentForm = server.forms.getForm('creditCard');
-
-    viewData.adyenEncryptedCardNumber = paymentForm.adyenEncryptedCardNumber.value;
-    viewData.adyenEncryptedExpiryMonth = paymentForm.adyenEncryptedExpiryMonth.value;
-    viewData.adyenEncryptedExpiryYear = paymentForm.adyenEncryptedExpiryYear.value;
-    viewData.adyenEncryptedSecurityCode = paymentForm.adyenEncryptedSecurityCode.value;
-    viewData.browserInfo = paymentForm.browserInfo.value;
-    res.setViewData(viewData);
-
-    if (paymentForm.valid) {
-        this.on('route:BeforeComplete', function (req, res) {
-            var URLUtils = require('dw/web/URLUtils');
-            var CustomerMgr = require('dw/customer/CustomerMgr');
-            var Resource = require('dw/web/Resource');
-
-            var customer = CustomerMgr.getCustomerByCustomerNumber(
-                req.currentCustomer.profile.customerNo
-            );
-
-            var createRecurringPaymentAccountResult = AdyenHelper.createRecurringPaymentAccount({
-                Customer: customer
-            });
-
-            if (createRecurringPaymentAccountResult.error) {
-                res.json({
-                    success: false,
-                    error: [Resource.msg('error.card.information.error', 'creditCard', null)]
-                });
-            } else {
-                res.json({
-                    success: true,
-                    redirectUrl: URLUtils.url('PaymentInstruments-List').toString()
-                });
-            }
-        });
-    } else {
-        res.json({
-            success: false,
-            error: [Resource.msg('error.card.information.error', 'creditCard', null)]
-        });
-    }
-    return next();
-});
+// server.replace('SavePayment', csrfProtection.validateAjaxRequest, function (req, res, next) {
+//     var viewData = res.getViewData();
+//     var paymentForm = server.forms.getForm('creditCard');
+//
+//     viewData.adyenEncryptedCardNumber = paymentForm.adyenEncryptedCardNumber.value;
+//     viewData.adyenEncryptedExpiryMonth = paymentForm.adyenEncryptedExpiryMonth.value;
+//     viewData.adyenEncryptedExpiryYear = paymentForm.adyenEncryptedExpiryYear.value;
+//     viewData.adyenEncryptedSecurityCode = paymentForm.adyenEncryptedSecurityCode.value;
+//     viewData.browserInfo = paymentForm.browserInfo.value;
+//     res.setViewData(viewData);
+//
+//     if (paymentForm.valid) {
+//         this.on('route:BeforeComplete', function (req, res) {
+//             var URLUtils = require('dw/web/URLUtils');
+//             var CustomerMgr = require('dw/customer/CustomerMgr');
+//             var Resource = require('dw/web/Resource');
+//
+//             var customer = CustomerMgr.getCustomerByCustomerNumber(
+//                 req.currentCustomer.profile.customerNo
+//             );
+//
+//             var createRecurringPaymentAccountResult = AdyenHelper.createRecurringPaymentAccount({
+//                 Customer: customer
+//             });
+//
+//             if (createRecurringPaymentAccountResult.error) {
+//                 res.json({
+//                     success: false,
+//                     error: [Resource.msg('error.card.information.error', 'creditCard', null)]
+//                 });
+//             } else {
+//                 res.json({
+//                     success: true,
+//                     redirectUrl: URLUtils.url('PaymentInstruments-List').toString()
+//                 });
+//             }
+//         });
+//     } else {
+//         res.json({
+//             success: false,
+//             error: [Resource.msg('error.card.information.error', 'creditCard', null)]
+//         });
+//     }
+//     return next();
+// });
 
 server.append('DeletePayment', userLoggedIn.validateLoggedInAjax, function (req, res, next) {
     var CustomerMgr = require('dw/customer/CustomerMgr');
