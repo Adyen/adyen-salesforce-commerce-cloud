2a6f107034f0e676b5a1a2e08773f3a361978086
zaid <zaid.dreakh@adyen.com>
2020-03-06 09:25:01 +0100

    renamed test file

---
+++ src/cartridges/int_adyen_SFRA/test/unit/scripts/checkout/adyenCheckout.test.js
@@ -0,0 +1,8 @@
+const adyenCheckout = require('../../../../cartridge/client/default/js/adyenCheckout');
+window.jQuery = window.$ = require('jquery');
+
+describe('rendering components', () => {
+    it('', () => {
+    });
+});
+
