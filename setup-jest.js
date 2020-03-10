29e07636fad93b8f8bd0a8a6998d0c9e2d798a94
zaid <zaid.dreakh@adyen.com>
2020-03-10 11:01:47 +0100

    review comments

--- setup-jest.js
+++ setup-jest.js
@@ -1,14 +1,10 @@
-//importing jquery
 import $ from 'jquery';
 global.$ = global.jQuery = $;
 
-//mocking "checkout" and its functions "mount" and "create"
 const mount = jest.fn();
 const create = jest.fn(() => ({ mount }));
 
-//mocking "AdyenCheckout" and its function "create"
 global.AdyenCheckout = jest.fn(() => ({create}));
 
-//mocking "showStoreDetails"
 global.showStoreDetails = false;
 
--- src/cartridges/int_adyen_SFRA/test/unit/scripts/checkout/adyenCheckout.test.js
+++ src/cartridges/int_adyen_SFRA/test/unit/scripts/checkout/adyenCheckout.test.js
@@ -1,4 +1,3 @@
-const adyenCheckout = require('../../../../cartridge/client/default/js/adyenCheckout');
 window.jQuery = window.$ = require('jquery');
 
 describe('rendering components', () => {
