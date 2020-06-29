79d8435d0689f4c9b91b62b2acaa18bc88b21813
KadoBOT <ricardo.ambrogi@gmail.com>
2020-06-29 14:04:55 +0200

    update payment model

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,7 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-        presets: [
-            [
-                '@babel/preset-env',
-                {
-                    targets: {
-                        esmodules: true,
-                    },
-                },
-            ],
-        ],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [["@babel/preset-env"]],
+    plugins: ["@babel/plugin-proposal-optional-chaining"],
+  };
 };
