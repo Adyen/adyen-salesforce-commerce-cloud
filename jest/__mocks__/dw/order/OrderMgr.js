441ac89008a573d2becab92c5fcfbf4f537773b0
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-07-27 15:52:27 +0200

    Unit tests for redirect middleware (#317)

--- jest/__mocks__/dw/order/OrderMgr.js
+++ jest/__mocks__/dw/order/OrderMgr.js
@@ -1,15 +1,33 @@
-const paymentInstrument = () => ({
-  custom: {
-    adyenPaymentData: "mocked_adyen_payment_data",
+const paymentInstrument = () => [
+  {
+    custom: {
+      adyenPaymentData: 'mocked_adyen_payment_data',
+    },
   },
-});
-export const getPaymentInstruments = jest.fn(() => [paymentInstrument()]);
+];
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.iterator = function iterator() {
+  return {
+    val: paymentInstrument(),
+    next() {
+      const prev = this.val[0];
+      this.val = null;
+      return prev;
+    },
+    hasNext() {
+      return !!this.val;
+    },
+  };
+};
+
+export const getPaymentInstruments = jest.fn(paymentInstrument);
 export const getOrder = jest.fn((/* orderNo */) => ({
   getPaymentInstruments,
   setPaymentStatus: jest.fn(),
   setExportStatus: jest.fn(),
-  orderNo: "mocked_orderNo",
-  orderToken: "mocked_orderToken",
+  orderNo: 'mocked_orderNo',
+  orderToken: 'mocked_orderToken',
 }));
 
 export const failOrder = jest.fn((orderNo, bool) => ({
