e69dd32cdcffbe58e19fc8b9db118075d05df619
KadoBOT <ricardo.ambrogi@gmail.com>
2020-07-30 12:42:02 +0200

    unit tests for confirm

--- jest/__mocks__/*/cartridge/scripts/util/adyenHelper.js
+++ jest/__mocks__/*/cartridge/scripts/util/adyenHelper.js
@@ -4,3 +4,17 @@ export const getAdyenHash = jest.fn((str) => str);
 export const getLoadingContext = jest.fn(() => 'mocked_loading_context');
 export const getCreditCardInstallments = jest.fn(() => true);
 export const getCurrencyValueForApi = jest.fn(() => 1000);
+export const getAdyenGivingEnabled = jest.fn(() => true);
+export const isAdyenGivingAvailable = jest.fn(() => true);
+export const getDonationAmounts = jest.fn(() => [10, 20, 30]);
+export const getAdyenGivingCharityName = jest.fn(() => 'mocked_charity_name');
+export const getAdyenGivingCharityWebsite = jest.fn(
+  () => 'mocked_charity_website',
+);
+export const getAdyenGivingCharityDescription = jest.fn(
+  () => 'mocked_charity_description',
+);
+export const getAdyenGivingBackgroundUrl = jest.fn(
+  () => 'mocked_background_url',
+);
+export const getAdyenGivingLogoUrl = jest.fn(() => 'mocked_logo_url');
