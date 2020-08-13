fece26b8d10ba1e879a2dda4770bbdb7785c24b4
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-08-13 07:22:47 +0200

    refactor show confirmation (#339)

--- src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/adyen/showConfirmation.js
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/adyen/showConfirmation.js
@@ -12,11 +12,70 @@ const OrderModel = require('*/cartridge/models/order');
 const { clearForms } = require('../../utils/index');
 
 function showConfirmation(req, res, next) {
-  try {
-    const order = OrderMgr.getOrder(session.privacy.orderNo);
-    const paymentInstruments = order.getPaymentInstruments(
-      constants.METHOD_ADYEN_COMPONENT,
+  function handleRedirect(page) {
+    res.redirect(
+      URLUtils.url(
+        'Checkout-Begin',
+        'stage',
+        page,
+        'paymentError',
+        Resource.msg('error.payment.not.valid', 'checkout', null),
+      ),
     );
+  }
+
+  function handleReceived(order, result) {
+    Transaction.wrap(() => {
+      OrderMgr.failOrder(order, true);
+    });
+    Logger.getLogger('Adyen').error(
+      `Did not complete Alipay transaction, result: ${JSON.stringify(result)}`,
+    );
+    res.redirect(
+      URLUtils.url(
+        'Checkout-Begin',
+        'stage',
+        'payment',
+        'paymentError',
+        Resource.msg('error.payment.not.valid', 'checkout', null),
+      ),
+    );
+    return next();
+  }
+
+  function handleOrderConfirm(
+    order,
+    orderModel,
+    adyenPaymentInstrument,
+    result,
+  ) {
+    Transaction.wrap(() => {
+      order.custom.Adyen_CustomerEmail = JSON.stringify(orderModel);
+      AdyenHelper.savePaymentDetails(adyenPaymentInstrument, order, result);
+    });
+
+    clearForms();
+    res.redirect(
+      URLUtils.url(
+        'Order-Confirm',
+        'ID',
+        order.orderNo,
+        'token',
+        order.orderToken,
+      ).toString(),
+    );
+    return next();
+  }
+
+  function handlePaymentError(order, page) {
+    Transaction.wrap(() => {
+      OrderMgr.failOrder(order, true);
+    });
+    handleRedirect(page);
+    return next();
+  }
+
+  function handlePaymentInstruments(paymentInstruments) {
     let adyenPaymentInstrument;
     let paymentData;
     let details;
@@ -34,6 +93,50 @@ function showConfirmation(req, res, next) {
     } else if (req.querystring.payload) {
       details = { payload: req.querystring.payload };
     }
+    return { details, paymentData, adyenPaymentInstrument };
+  }
+
+  function handleAuthorised(order, result, adyenPaymentInstrument) {
+    if (
+      result.resultCode === 'Received' &&
+      result.paymentMethod.indexOf('alipay_hk') > -1
+    ) {
+      return handleReceived(order, result);
+    }
+
+    // custom fraudDetection
+    const fraudDetectionStatus = { status: 'success' };
+
+    // Places the order
+    const placeOrderResult = COHelpers.placeOrder(order, fraudDetectionStatus);
+    if (placeOrderResult.error) {
+      return handlePaymentError(order, 'placeOrder');
+    }
+
+    const currentLocale = Locale.getLocale(req.locale.id);
+    const orderModel = new OrderModel(order, {
+      countryCode: currentLocale.country,
+    });
+
+    // Save orderModel to custom object during session
+    return handleOrderConfirm(
+      order,
+      orderModel,
+      adyenPaymentInstrument,
+      result,
+    );
+  }
+
+  try {
+    const order = OrderMgr.getOrder(session.privacy.orderNo);
+    const paymentInstruments = order.getPaymentInstruments(
+      constants.METHOD_ADYEN_COMPONENT,
+    );
+    const {
+      details,
+      paymentData,
+      adyenPaymentInstrument,
+    } = handlePaymentInstruments(paymentInstruments);
 
     // redirect to payment/details
     const requestObject = {
@@ -47,95 +150,10 @@ function showConfirmation(req, res, next) {
     });
 
     // Authorised: The payment authorisation was successfully completed.
-    if (
-      result.resultCode === 'Authorised' ||
-      result.resultCode === 'Pending' ||
-      result.resultCode === 'Received'
-    ) {
-      if (
-        result.resultCode === 'Received' &&
-        result.paymentMethod.indexOf('alipay_hk') > -1
-      ) {
-        Transaction.wrap(() => {
-          OrderMgr.failOrder(order, true);
-        });
-        Logger.getLogger('Adyen').error(
-          `Did not complete Alipay transaction, result: ${JSON.stringify(
-            result,
-          )}`,
-        );
-        res.redirect(
-          URLUtils.url(
-            'Checkout-Begin',
-            'stage',
-            'payment',
-            'paymentError',
-            Resource.msg('error.payment.not.valid', 'checkout', null),
-          ),
-        );
-        return next();
-      }
-
-      // custom fraudDetection
-      const fraudDetectionStatus = { status: 'success' };
-
-      // Places the order
-      const placeOrderResult = COHelpers.placeOrder(
-        order,
-        fraudDetectionStatus,
-      );
-      if (placeOrderResult.error) {
-        Transaction.wrap(() => {
-          OrderMgr.failOrder(order, true);
-        });
-        res.redirect(
-          URLUtils.url(
-            'Checkout-Begin',
-            'stage',
-            'placeOrder',
-            'paymentError',
-            Resource.msg('error.payment.not.valid', 'checkout', null),
-          ),
-        );
-        return next();
-      }
-
-      const currentLocale = Locale.getLocale(req.locale.id);
-      const orderModel = new OrderModel(order, {
-        countryCode: currentLocale.country,
-      });
-
-      // Save orderModel to custom object during session
-      Transaction.wrap(() => {
-        order.custom.Adyen_CustomerEmail = JSON.stringify(orderModel);
-        AdyenHelper.savePaymentDetails(adyenPaymentInstrument, order, result);
-      });
-
-      clearForms();
-      res.redirect(
-        URLUtils.url(
-          'Order-Confirm',
-          'ID',
-          order.orderNo,
-          'token',
-          order.orderToken,
-        ).toString(),
-      );
-      return next();
+    if (['Authorised', 'Pending', 'Received'].indexOf(result.resultCode) > -1) {
+      return handleAuthorised(order, result, adyenPaymentInstrument);
     }
-    Transaction.wrap(() => {
-      OrderMgr.failOrder(order, true);
-    });
-    res.redirect(
-      URLUtils.url(
-        'Checkout-Begin',
-        'stage',
-        'placeOrder',
-        'paymentError',
-        Resource.msg('error.payment.not.valid', 'checkout', null),
-      ),
-    );
-    return next();
+    return handlePaymentError(order, 'placeOrder');
   } catch (e) {
     Logger.getLogger('Adyen').error(
       `Could not verify /payment/details: ${e.message}`,
