fdf7e61e4939bf5b90f883bdd20e744ba7957c04
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2020-08-17 00:01:51 +0200

    unit tests for POS script files

---
+++ src/cartridges/int_adyen_SFRA/cartridge/scripts/hooks/payment/processor/middlewares/pos_handle.js
@@ -0,0 +1,21 @@
+const Transaction = require('dw/system/Transaction');
+const collections = require('*/cartridge/scripts/util/collections');
+const constants = require('*/cartridge/adyenConstants/constants');
+
+function pos_handle(basket /* , paymentInformation */) {
+    Transaction.wrap(() => {
+        collections.forEach(basket.getPaymentInstruments(), (item) => {
+            basket.removePaymentInstrument(item);
+        });
+
+        const paymentInstrument = basket.createPaymentInstrument(
+            constants.METHOD_ADYEN_POS,
+            basket.totalGrossPrice,
+        );
+        paymentInstrument.custom.adyenPaymentMethod = 'POS Terminal';
+    });
+
+    return { error: false };
+}
+
+module.exports = pos_handle;
