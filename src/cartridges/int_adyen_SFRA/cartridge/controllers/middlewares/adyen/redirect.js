4a13f7a7572680c45313ec25e22b6f40e3322348
Ricardo Ambrogi <ricardo.ambrogi@gmail.com>
2020-09-18 22:18:04 +0200

    refactor redirect (#366)

--- src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/adyen/redirect.js
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/adyen/redirect.js
@@ -1,58 +1,19 @@
 const OrderMgr = require('dw/order/OrderMgr');
 const Logger = require('dw/system/Logger');
-const Transaction = require('dw/system/Transaction');
-const URLUtils = require('dw/web/URLUtils');
-const Resource = require('dw/web/Resource');
-const constants = require('*/cartridge/adyenConstants/constants');
-const AdyenHelper = require('*/cartridge/scripts/util/adyenHelper');
+const {
+  getCurrentSignature,
+  handleIncorrectSignature,
+} = require('./redirect/signature');
 
 function redirect(req, res, next) {
   const order = OrderMgr.getOrder(session.privacy.orderNo);
-  function handleIncorrectSignature() {
-    Logger.getLogger('Adyen').error('Redirect signature is not correct');
-    Transaction.wrap(() => {
-      OrderMgr.failOrder(order, true);
-    });
-    res.redirect(
-      URLUtils.url(
-        'Checkout-Begin',
-        'stage',
-        'payment',
-        'paymentError',
-        Resource.msg('error.payment.not.valid', 'checkout', null),
-      ),
-    );
-    return next();
-  }
 
-  function getCurrentSignature() {
-    const paymentInstruments = order.getPaymentInstruments(
-      constants.METHOD_ADYEN_COMPONENT,
-    );
-    let adyenPaymentInstrument;
-    let paymentData;
-
-    // looping through all Adyen payment methods, however, this only can be one.
-    const instrumentsIter = paymentInstruments.iterator();
-    while (instrumentsIter.hasNext()) {
-      adyenPaymentInstrument = instrumentsIter.next();
-      paymentData = adyenPaymentInstrument.custom.adyenPaymentData;
-    }
-    const currentSignature = AdyenHelper.getAdyenHash(
-      req.querystring.redirectUrl.substr(
-        req.querystring.redirectUrl.length - 25,
-      ),
-      paymentData.substr(1, 25),
-    );
-    return currentSignature;
-  }
-
-  const { signature } = req.querystring;
+  const { signature, redirectUrl } = req.querystring;
   if (order && signature) {
-    const currentSignature = getCurrentSignature();
+    const currentSignature = getCurrentSignature(order, { req });
 
     if (signature === currentSignature) {
-      res.redirect(req.querystring.redirectUrl);
+      res.redirect(redirectUrl);
       return next();
     }
   } else {
@@ -61,7 +22,7 @@ function redirect(req, res, next) {
     );
   }
 
-  return handleIncorrectSignature();
+  return handleIncorrectSignature(order, { res, next });
 }
 
 module.exports = redirect;
---
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/adyen/redirect/signature.js
@@ -0,0 +1,42 @@
+const OrderMgr = require('dw/order/OrderMgr');
+const Logger = require('dw/system/Logger');
+const Transaction = require('dw/system/Transaction');
+const URLUtils = require('dw/web/URLUtils');
+const Resource = require('dw/web/Resource');
+const constants = require('*/cartridge/adyenConstants/constants');
+const AdyenHelper = require('*/cartridge/scripts/util/adyenHelper');
+
+function handleIncorrectSignature(order, { res, next }) {
+  Logger.getLogger('Adyen').error('Redirect signature is not correct');
+  Transaction.wrap(() => {
+    OrderMgr.failOrder(order, true);
+  });
+  res.redirect(
+    URLUtils.url(
+      'Checkout-Begin',
+      'stage',
+      'payment',
+      'paymentError',
+      Resource.msg('error.payment.not.valid', 'checkout', null),
+    ),
+  );
+  return next();
+}
+
+function getCurrentSignature(order, { req }) {
+  const paymentInstruments = order.getPaymentInstruments(
+    constants.METHOD_ADYEN_COMPONENT,
+  );
+  const adyenPaymentInstrument = paymentInstruments[0];
+  const paymentData = adyenPaymentInstrument.custom.adyenPaymentData;
+
+  return AdyenHelper.getAdyenHash(
+    req.querystring.redirectUrl.substr(req.querystring.redirectUrl.length - 25),
+    paymentData.substr(1, 25),
+  );
+}
+
+module.exports = {
+  handleIncorrectSignature,
+  getCurrentSignature,
+};
