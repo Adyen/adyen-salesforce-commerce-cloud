e13c76c67d3290cf77e66e045bc4f370a7f371c2
KadoBOT <ricardo.ambrogi@gmail.com>
2020-09-21 06:38:55 +0200

    rename folders from snake_case to camelCase

--- src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/index.js
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/index.js
@@ -1,8 +1,8 @@
 const adyen = require('./adyen/index');
-const checkoutServices = require('./checkout_services/index');
+const checkoutServices = require('./checkoutServices/index');
 const checkout = require('./checkout/index');
 const order = require('./order/index');
-const paymentInstruments = require('./payment_instruments/index');
+const paymentInstruments = require('./paymentInstruments/index');
 
 module.exports = {
   adyen,
