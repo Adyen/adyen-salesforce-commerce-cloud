efa737089402e01b9e331dbd748b2a0c4dc4aaf8
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2020-10-22 21:32:32 +0200

    added amount to paypal config

--- src/cartridges/int_adyen_SFRA/cartridge/controllers/Adyen.js
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/Adyen.js
@@ -722,20 +722,18 @@ server.get('GetPaymentMethods', server.middleware.https, function (
   }
 
   const adyenURL = `${AdyenHelper.getLoadingContext()}images/logos/medium/`;
+  const paymentAmount = currentBasket.getTotalGrossPrice()
+      ? AdyenHelper.getCurrencyValueForApi(currentBasket.getTotalGrossPrice())
+      : 1000;
+  const currency = currentBasket.getTotalGrossPrice().currencyCode;
   const jsonResponse = {
     AdyenPaymentMethods: response,
     ImagePath: adyenURL,
     AdyenDescriptions: paymentMethodDescriptions,
     AdyenConnectedTerminals: JSON.parse(connectedTerminals),
+    amount: { value: paymentAmount, currency: currency },
+    countryCode,
   };
-  if (AdyenHelper.getCreditCardInstallments()) {
-    const paymentAmount = currentBasket.getTotalGrossPrice()
-      ? AdyenHelper.getCurrencyValueForApi(currentBasket.getTotalGrossPrice())
-      : 1000;
-    const currency = currentBasket.getTotalGrossPrice().currencyCode;
-    jsonResponse.amount = { value: paymentAmount, currency: currency };
-    jsonResponse.countryCode = countryCode;
-  }
 
   res.json(jsonResponse);
   return next();
