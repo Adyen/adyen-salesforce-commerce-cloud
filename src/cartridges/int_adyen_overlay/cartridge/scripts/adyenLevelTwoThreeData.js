0adaf95d395517b96544fb4ade2ca2088570ca1f
Zaid Dreakh <zaid.dreakh@hotmail.co.uk>
2020-11-19 15:03:06 +0100

    PW-3232 Bug fix for L2/3 data and package.json (#391)
    * level23 fixed

--- src/cartridges/int_adyen_overlay/cartridge/scripts/adyenLevelTwoThreeData.js
+++ src/cartridges/int_adyen_overlay/cartridge/scripts/adyenLevelTwoThreeData.js
@@ -48,15 +48,14 @@ function getLineItems({ Order: order }) {
     const itemAmount = LineItemHelper.getItemAmount(lineItem) / quantity;
     const vatAmount = LineItemHelper.getVatAmount(lineItem) / quantity;
     const commodityCode = AdyenHelper.getAdyenLevel23CommodityCode();
-
     const currentLineItem = {
-      [`enhancedSchemeData.itemDetailLine${index + 1}.description`]: description.substring(0, 26).replace(/[^\x00-\x7F]/g, ''),
       [`enhancedSchemeData.itemDetailLine${index + 1}.unitPrice`]: itemAmount.toFixed(),
       [`enhancedSchemeData.itemDetailLine${index + 1}.totalAmount`]: parseFloat(itemAmount.toFixed()) + parseFloat(vatAmount.toFixed()),
       [`enhancedSchemeData.itemDetailLine${index + 1}.quantity`]: quantity,
-      [`enhancedSchemeData.itemDetailLine${index + 1}.productCode`]: id.substring(0, 12),
       [`enhancedSchemeData.itemDetailLine${index + 1}.unitOfMeasure`]: 'EAC',
       ...(commodityCode && { [`enhancedSchemeData.itemDetailLine${index + 1}.commodityCode`]: commodityCode }),
+      ...(description && { [`enhancedSchemeData.itemDetailLine${index + 1}.description`]: description.substring(0, 26).replace(/[^\x00-\x7F]/g, '') }),
+      ...(id && { [`enhancedSchemeData.itemDetailLine${index + 1}.productCode`]: id.substring(0, 12) }),
     };
 
     return {
