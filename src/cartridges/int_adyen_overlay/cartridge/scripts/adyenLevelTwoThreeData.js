8b7f9bbb3839b9a391798e03c200fa1d9b2bd099
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2020-12-18 13:21:23 +0100

    deleted unecessary comments

--- src/cartridges/int_adyen_overlay/cartridge/scripts/adyenLevelTwoThreeData.js
+++ src/cartridges/int_adyen_overlay/cartridge/scripts/adyenLevelTwoThreeData.js
@@ -1,29 +1,3 @@
-/**
- * Generate the parameters needed for the redirect to the Adyen Hosted Payment Page.
- * A signature is calculated based on the configured HMAC code
- *
- * @input Order : dw.order.Order
- * @input OrderNo : String The order no
- * @input CurrentSession : dw.system.Session
- * @input CurrentUser : dw.customer.Customer
- * @input PaymentInstrument : dw.order.PaymentInstrument
- * @input brandCode : String
- * @input issuerId : String
- * @input dob : String
- * @input gender : String
- * @input telephoneNumber : String
- * @input houseNumber : String
- * @input houseExtension : String
- * @input socialSecurityNumber : String
- *
- * @output merchantSig : String;
- * @output Amount100 : String;
- * @output shopperEmail : String;
- * @output shopperReference : String;
- * @output paramsMap : dw.util.SortedMap;
- * @output sessionValidity : String;
- *
- */
 require('dw/crypto');
 require('dw/system');
 require('dw/order');
