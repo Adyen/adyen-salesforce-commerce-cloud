a997e73a8179096c522f07534ae131fbcf2c7152
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2021-02-16 18:18:02 +0100

    WIP

--- src/cartridges/int_adyen_SFRA/cartridge/scripts/adyenService.js
+++ src/cartridges/int_adyen_SFRA/cartridge/scripts/adyenService.js
@@ -4,8 +4,10 @@ const HashMap = require('dw/util/HashMap');
 const Mail = require('dw/net/Mail');
 const Template = require('dw/util/Template');
 const Transaction = require('dw/system/Transaction');
+const Order = require('dw/order/Order');
+const COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
 
-function submit(order) {
+function sendEmail(order) {
   const confirmationEmail = new Mail();
   const context = new HashMap();
 
@@ -30,9 +32,37 @@ function submit(order) {
   confirmationEmail.setContent(content, 'text/html', 'UTF-8');
   confirmationEmail.send();
 
-  Transaction.wrap(() => {
-    order.custom.Adyen_CustomerEmail = null;
-  });
+  order.custom.Adyen_CustomerEmail = null;
+}
+
+function submit(order) {
+  try {
+    Transaction.begin();
+    // Places the order if not placed yet
+    if (order.status === Order.ORDER_STATUS_CREATED) {
+      // custom fraudDetection
+      const fraudDetectionStatus = { status: 'success' };
+      const placeOrderResult = COHelpers.placeOrder(
+        order,
+        fraudDetectionStatus,
+      );
+      if (placeOrderResult.error) {
+        return placeOrderResult;
+      }
+    }
+
+    sendEmail(order);
+    Transaction.commit();
+
+    return {
+      order_created: true,
+    };
+  } catch (e) {
+    Transaction.rollback();
+    return {
+      error: true,
+    };
+  }
 }
 
 module.exports = {
--- src/cartridges/int_adyen_controllers/cartridge/scripts/adyenService.js
+++ src/cartridges/int_adyen_controllers/cartridge/scripts/adyenService.js
@@ -3,7 +3,7 @@ const app = require('app_storefront_controllers/cartridge/scripts/app');
 const OrderModel = app.getModel('Order');
 
 function submit(order) {
-  OrderModel.submit(order);
+  return OrderModel.submit(order);
 }
 
 module.exports = {
