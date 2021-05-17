88c74dc3ba4c2c9cef66004f7e077aa3f9caf84e
wouterboe <wouter.boereboom@adyen.com>
2021-05-17 16:51:21 +0200

    WIP

--- src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyen_checkout/checkoutConfiguration.js
+++ src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyen_checkout/checkoutConfiguration.js
@@ -164,6 +164,29 @@ function handleOnChange(state) {
   store.componentsObj[type].stateData = state.data;
 }
 
+function getAmazonpayConfig() {
+  return {
+    showPayButton: true,
+    productType: 'PayOnly',
+    checkoutMode: 'ProcessOrder',
+    returnUrl: window.location.href,
+    configuration: {
+      merchantId: "AAUL9GPRGTX1U",
+      storeId: "amzn1.application-oa2-client.3e5db0a580f7468da2d9903dda981fce",
+      publicKeyId: "AGDRUNN37LQHSOCHN24AEYYB"
+    },
+    "amount": {
+      value: "88800",
+      currency: "GBP"
+    },
+    // onSubmit: () => {
+    //   helpers.assignPaymentMethodValue();
+    //   document.querySelector('button[value="submit-payment"]').disabled = false;
+    //   document.querySelector('button[value="submit-payment"]').click();
+    // },
+  };
+}
+
 function setCheckoutConfiguration() {
   store.checkoutConfiguration.onChange = handleOnChange;
   store.checkoutConfiguration.showPayButton = false;
@@ -188,26 +211,7 @@ function setCheckoutConfiguration() {
     swish: getQRCodeConfig(),
     bcmc_mobile: getQRCodeConfig(),
     wechatpayQR: getQRCodeConfig(),
-    amazonpay: {
-      showPayButton: true,
-      productType: 'PayOnly',
-      checkoutMode: 'ProcessOrder',
-      returnUrl: "https://zzft-004.sandbox.us01.dx.commercecloud.salesforce.com/on/demandware.store/Sites-RefArch-Site/fr_FR/Checkout-Begin?stage=placeOrder",
-      configuration: {
-        merchantId: "AAUL9GPRGTX1U",
-        storeId: "amzn1.application-oa2-client.3e5db0a580f7468da2d9903dda981fce",
-        publicKeyId: "AGDRUNN37LQHSOCHN24AEYYB"
-      },
-      "amount": {
-        value: "88800",
-        currency: "GBP"
-      },
-      // onSubmit: () => {
-      //   helpers.assignPaymentMethodValue();
-      //   document.querySelector('button[value="submit-payment"]').disabled = false;
-      //   document.querySelector('button[value="submit-payment"]').click();
-      // },
-    },
+    amazonpay: getAmazonpayConfig(),
     afterpay_default: {
       visibility: {
         personalDetails: 'editable',
--- src/cartridges/int_adyen_SFRA/cartridge/client/default/js/amazon.js
+++ src/cartridges/int_adyen_SFRA/cartridge/client/default/js/amazon.js
@@ -2,21 +2,46 @@ const amazonPayNode = document.getElementById('amazon-container');
 
 console.log(window.amazonCheckoutSessionId)
 const amazonConfig = {
-  showPayButton: true,
-  // productType: 'PayOnly',
-  // checkoutMode: 'ProcessOrder',
+  showPayButton: false,
+  productType: 'PayOnly',
+  checkoutMode: 'ProcessOrder',
   returnUrl: window.returnURL,
   // configuration: {
   //   merchantId: "AAUL9GPRGTX1U",
   //   storeId: "amzn1.application-oa2-client.3e5db0a580f7468da2d9903dda981fce",
   //   publicKeyId: "AGDRUNN37LQHSOCHN24AEYYB"
   // },
-  amazonCheckoutSessionId: window.amazonCheckoutSessionId,
   amount: {
     value: "88800",
     currency: "GBP"
   },
+  amazonCheckoutSessionId: window.amazonCheckoutSessionId,
+  onSubmit: (state, component) => {
+    // assignPaymentMethodValue();
+    console.log("Submit amazon pay 2nd call");
+    document.querySelector('#adyenStateData').value = JSON.stringify(
+        state.data,
+    );
+    $('#dwfrm_billing').trigger('submit');
+    // paymentFromComponent(state.data, component);
+  },
 };
+
+// $('#dwfrm_billing').submit(function (e) {
+//     e.preventDefault();
+//     var form = $(this);
+//     var url = form.attr('action');
+//
+//     $.ajax({
+//       type: 'POST',
+//       url: url,
+//       data: form.serialize(),
+//       async: false,
+//       success: function (data) {
+//         formErrorsExist = data.fieldErrors;
+//       },
+//     });
+// });
 
 const checkout = new AdyenCheckout(window.Configuration);
 checkout.create('amazonpay', amazonConfig).mount(amazonPayNode);
