54afd8a9a0dedb13755d47da083bd1e9523d4ed4
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-07-09 13:55:11 +0200

    updated jest objects to match 3ds1 fix

--- jest/__mocks__/*/cartridge/scripts/adyenCheckout.js
+++ jest/__mocks__/*/cartridge/scripts/adyenCheckout.js
@@ -1,6 +1,12 @@
-export const doPaymentsDetailsCall = jest.fn(({ paymentData }) => ({
-  resultCode: paymentData,
-}));
+export const doPaymentsDetailsCall = jest.fn((payload) => {
+  let resultCode;
+  if(payload.paymentData) {
+    resultCode = payload.paymentData;
+  } else if(payload.details?.MD) {
+    resultCode = payload.details.MD === 'mocked_md' ? 'Authorised': 'Not_Authorised';
+  }
+  return {resultCode};
+});
 export const createPaymentRequest = jest.fn(() => ({
   resultCode: 'Authorised',
 }));
