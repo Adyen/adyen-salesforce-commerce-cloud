66877458a359809b244f31bc15128cafe72389e4
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2021-07-28 17:07:03 +0200

    updated cartridges

--- cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/order/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var confirm = require('./confirm');
-
-module.exports = {
-  confirm: confirm
-};
--- cartridges/int_adyen_controllers/cartridge/js/adyenConstants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// Adyen constants
-module.exports = {
-  METHOD_ADYEN: 'Adyen',
-  METHOD_ADYEN_POS: 'AdyenPOS',
-  METHOD_CREDIT_CARD: 'CREDIT_CARD'
-};
