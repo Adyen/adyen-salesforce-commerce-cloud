e7615430715240a26acc15cefc2a26f5ae621e42
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-08-10 09:21:41 +0200

    merged master into develop_release2110

--- src/cartridges/int_adyen_SFRA/test/integration/config.js
+++ src/cartridges/int_adyen_SFRA/test/integration/config.js
@@ -1,34 +1,34 @@
-const cvcFail = "123";
-const cvcSuccess = "737";
-const expiryDate = "1020";
-const mastercard = "5100290029002909";
-const mastercard3D = "5454545454545454";
-const holdernameSuccess = "Mastercard Success";
-const holdernameFail = "Mastercard Fail";
-const username = "YOUR_USERNAME";
-const password = "YOUR_PASSWORD";
-const password3d = "password";
-const passwordFail = "passwordFail";
+const cvcFail = '123';
+const cvcSuccess = '737';
+const expiryDate = '1020';
+const mastercard = '5100290029002909';
+const mastercard3D = '5454545454545454';
+const holdernameSuccess = 'Mastercard Success';
+const holdernameFail = 'Mastercard Fail';
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+const password3d = 'password';
+const passwordFail = 'passwordFail';
 
 module.exports = {
   Storefront: {
-    url: "https://www.yourstorefront.com?lang=en_US",
-    login: "/on/demandware.store/Sites-RefArch-Site/default/Checkout-Login",
-    urlEUR: "https://www.yourstorefront.com?lang=fr_FR",
-    loginEUR: "/on/demandware.store/Sites-RefArch-Site/fr_FR/Checkout-Login",
+    url: 'https://www.yourstorefront.com?lang=en_US',
+    login: '/on/demandware.store/Sites-RefArch-Site/default/Checkout-Login',
+    urlEUR: 'https://www.yourstorefront.com?lang=fr_FR',
+    loginEUR: '/on/demandware.store/Sites-RefArch-Site/fr_FR/Checkout-Login',
   },
 
   Guest: {
-    guestEmail: "guest@adyen.com",
-    guestFirstName: "Guest",
-    guestLastName: "Test",
-    guestStreet: "Guest street 1",
-    guestHouseNumber: "123",
-    guestCity: "Amsterdam",
-    guestPostCode: "12321",
-    guestCountryCode: "NL",
-    guestPhoneNumber: "0612345679",
-    guestDOB: "1990-10-10",
+    guestEmail: 'guest@adyen.com',
+    guestFirstName: 'Guest',
+    guestLastName: 'Test',
+    guestStreet: 'Guest street 1',
+    guestHouseNumber: '123',
+    guestCity: 'Amsterdam',
+    guestPostCode: '12321',
+    guestCountryCode: 'NL',
+    guestPhoneNumber: '0612345679',
+    guestDOB: '1990-10-10',
   },
 
   cardSuccess: {
