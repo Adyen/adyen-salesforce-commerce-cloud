a4488635805110749b60f05ceda84f8903a90e25
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2021-08-11 10:56:42 +0200

    removed unecessary lines

--- src/cartridges/int_adyen_controllers/cartridge/scripts/adyenSaveCreditCard.js
+++ src/cartridges/int_adyen_controllers/cartridge/scripts/adyenSaveCreditCard.js
@@ -2,8 +2,6 @@ const app = require('app_storefront_controllers/cartridge/scripts/app');
 const Transaction = require('dw/system/Transaction');
 const constants = require('*/cartridge/adyenConstants/constants');
 const adyenZeroAuth = require('*/cartridge/scripts/adyenZeroAuth');
-const AdyenHelper = require('*/cartridge/scripts/util/adyenHelper');
-const Logger = require('dw/system/Logger');
 
 function create() {
   const paymentInformation = app.getForm('adyPaydata');
@@ -24,9 +22,7 @@ function create() {
     customer,
     paymentInstrument,
   );
-  Logger.getLogger('Adyen').error('zeroAuthResult ' + JSON.stringify(zeroAuthResult));
 
-  // const response = AdyenHelper.createAdyenCheckoutResponse(zeroAuthResult);
   if(zeroAuthResult.action) {
     return zeroAuthResult.action;
   }
