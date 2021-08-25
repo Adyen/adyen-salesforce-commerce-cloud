a91860378a6b1ea404840d7ad74d7eb57e5e0584
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2021-08-25 17:03:17 +0200

    fixed test

--- src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyen_checkout/renderGenericComponent.js
+++ src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyen_checkout/renderGenericComponent.js
@@ -98,18 +98,19 @@ function setCheckoutConfiguration(data) {
 
 function setAmazonPayConfig(adyenPaymentMethods) {
   const amazonpay = adyenPaymentMethods.paymentMethods.find(
-      (paymentMethod) => paymentMethod.type === 'amazonpay',
+    (paymentMethod) => paymentMethod.type === 'amazonpay',
   );
-  if(amazonpay) {
-    store.checkoutConfiguration.paymentMethodsConfiguration.amazonpay.configuration = amazonpay.configuration; // eslint-disable-line max-len
+  if (amazonpay) {
+    store.checkoutConfiguration.paymentMethodsConfiguration.amazonpay.configuration =
+      amazonpay.configuration; // eslint-disable-line max-len
     store.checkoutConfiguration.paymentMethodsConfiguration.amazonpay.addressDetails = {
       name: `${document.querySelector('#shippingFirstNamedefault').value} ${
-          document.querySelector('#shippingLastNamedefault').value
+        document.querySelector('#shippingLastNamedefault').value
       }`,
-      addressLine1: document.querySelector('#shippingFirstNamedefault').value,
+      addressLine1: document.querySelector('#shippingAddressOnedefault').value,
       city: document.querySelector('#shippingAddressCitydefault').value,
       stateOrRegion: document.querySelector('#shippingAddressCitydefault')
-          .value,
+        .value,
       postalCode: document.querySelector('#shippingZipCodedefault').value,
       countryCode: document.querySelector('#shippingCountrydefault').value,
       phoneNumber: document.querySelector('#shippingPhoneNumberdefault').value,
