9f542f9f03a3fa20b068e0f10124b07b0d23cf9b
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2021-08-27 11:12:37 +0200

    fix potential null in payment processors

--- src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/payment_instruments/savePayment.js
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/payment_instruments/savePayment.js
@@ -44,12 +44,19 @@ function getResponseBody(action) {
   };
 }
 
+function isAdyen() {
+  return (
+    paymentProcessorIDs.indexOf(
+      PaymentMgr.getPaymentMethod('CREDIT_CARD')
+        ?.getPaymentProcessor()
+        ?.getID(),
+    ) > -1 ||
+    PaymentMgr.getPaymentMethod(constants.METHOD_ADYEN_COMPONENT).isActive()
+  );
+}
+
 function savePayment(req, res, next) {
-  if (
-      paymentProcessorIDs.indexOf(
-      PaymentMgr.getPaymentMethod('CREDIT_CARD').getPaymentProcessor().getID(),
-    ) === -1
-  ) {
+  if (!isAdyen()) {
     return next();
   }
   const customer = CustomerMgr.getCustomerByCustomerNumber(
