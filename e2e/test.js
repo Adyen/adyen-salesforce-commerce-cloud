e111bdcb689314a8ee6d9817fadfc15a1e5c18a8
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-10-12 14:04:01 +0200

    testing e2e Hello world

--- e2e/test.js
+++ e2e/test.js
@@ -4,7 +4,7 @@ fixture `My first fixtures`
     .page `https://zzft-010.sandbox.us01.dx.commercecloud.salesforce.com/s/RefArch/home?lang=en_US`
     .httpAuth({
       username: process.env.SANDBOX_HTTP_AUTH_USERNAME,
-      password: process.env.SANDBOX_HTTP_AUTH_USERNAME,
+      password: process.env.SANDBOX_HTTP_AUTH_PASSWORD,
     });
 
 test('Hello world', async t => {
