b2c93d17c3a0276dad7b48416874df0a3c134ec9
basm <bas.maassen@adyen.com>
2021-10-15 16:37:25 +0200

    Enums fix

--- tests/e2e/fixtures/EUR.js
+++ tests/e2e/fixtures/EUR.js
@@ -1,9 +1,9 @@
 import checkout from "../pages/CheckoutPage";
 import NL from "./countriesEUR/NL";
-import { regionEnum } from "../data/enums"
+import { regionsEnum } from "../data/enums"
 //TODO check if we want to move URL
 fixture`EUR`
-    .page(`https://${process.env.SFCC_HOSTNAME}/s/RefArch/home?lang=${regionEnum.EU}`)
+    .page(`https://${process.env.SFCC_HOSTNAME}/s/RefArch/home?lang=${regionsEnum.EU}`)
     .httpAuth({
         username: process.env.SANDBOX_HTTP_AUTH_USERNAME,
         password: process.env.SANDBOX_HTTP_AUTH_PASSWORD,
