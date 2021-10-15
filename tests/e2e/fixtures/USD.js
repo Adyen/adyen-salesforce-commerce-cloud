56eb86f45e95bb39258d6aa412e11b8ca6a6de8e
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-10-15 16:31:28 +0200

    update fixture URLS

--- tests/e2e/fixtures/USD.js
+++ tests/e2e/fixtures/USD.js
@@ -218,7 +218,7 @@ test('my account remove card success', async () => {
 }
 =======
 fixture`USD`
-    .page(process.env.STOREFRONT_USD)
+    .page(`https://${process.env.SFCC_HOSTNAME}/s/RefArch/home?lang=${regionEnum.US}`)
     .httpAuth({
         username: process.env.SANDBOX_HTTP_AUTH_USERNAME,
         password: process.env.SANDBOX_HTTP_AUTH_PASSWORD,
