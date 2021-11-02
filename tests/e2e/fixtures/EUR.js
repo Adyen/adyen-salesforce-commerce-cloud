8c8b8ec08f16990b0748f411ebf3d812a8fce680
Bas Maassen <bas.maassen@adyen.com>
2021-11-02 14:16:22 +0100

    Update tests/e2e/fixtures/EUR.js
    Co-authored-by: Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>

--- tests/e2e/fixtures/EUR.js
+++ tests/e2e/fixtures/EUR.js
@@ -6,7 +6,7 @@ import testAustria from "./countriesEUR/AT";
 import { regionsEnum } from "../data/enums"
 
 fixture`EUR`
-    .page(`https://${process.env.SFCC_HOSTNAME}/s/RefArch/home?lang=${regionsEnum.EU}`)
+    .page(`https://${process.env.SFCC_HOSTNAME}/s/RefArch/home`)
     .httpAuth({
         username: process.env.SANDBOX_HTTP_AUTH_USERNAME,
         password: process.env.SANDBOX_HTTP_AUTH_PASSWORD,
