a13d4ec01ac57f9dfeb2d5807da1678722e1c2b0
Bas Maassen <bas.maassen@adyen.com>
2021-11-04 10:16:56 +0100

    Skip Affirm, EPS, Oney (#574)

--- tests/e2e/fixtures/countriesEUR/FR.js
+++ tests/e2e/fixtures/countriesEUR/FR.js
@@ -4,13 +4,13 @@ import { doOneyPayment } from "../../paymentFlows/redirectShopper"
 const shopperData = require("../../data/shopperData.json");
 const checkoutPage = new CheckoutPage();
 module.exports = () => {
-    test('Oney Success', async t => {
+    test.skip('Oney Success', async t => {
         await checkoutPage.setShopperDetails(shopperData.FR);
         await doOneyPayment(shopperData.FR);
         await checkoutPage.expectSuccess();
     });
 
-    test('Oney Fail', async t => {
+    test.skip('Oney Fail', async t => {
         await checkoutPage.setShopperDetails(shopperData.FR);
         await doOneyPayment(false);
         await checkoutPage.expectRefusal();
