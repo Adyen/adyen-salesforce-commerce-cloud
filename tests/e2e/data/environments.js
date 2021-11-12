944812d7d719c0f9d7b6da5e82602ff6da20253b
Wouter Boereboom <wouter.boereboom@adyen.com>
2021-11-12 15:32:44 +0100

    base for SG established

--- tests/e2e/data/environments.js
+++ tests/e2e/data/environments.js
@@ -1,10 +1,16 @@
 import CheckoutPageSFRA from "../pages/CheckoutPageSFRA";
+import CheckoutPageSG from "../pages/CheckoutPageSG";
 
 const environments = [
   {
     name: 'SFRA',
     CheckoutPage: CheckoutPageSFRA,
     urlExtension: '/s/RefArch/home',
+  },
+  {
+    name: 'SG',
+    CheckoutPage: CheckoutPageSG,
+    urlExtension: '/on/demandware.store/Sites-SiteGenesis-Site',
   }
 ]
 
