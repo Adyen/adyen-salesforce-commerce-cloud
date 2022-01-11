a87b9c67205140a8717f2228a18d380385cf509e
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2022-01-11 14:15:55 +0100

    lint

--- src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/checkout_services/adyenCheckoutServices.js
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/checkout_services/adyenCheckoutServices.js
@@ -2,38 +2,41 @@ const constants = require('*/cartridge/adyenConstants/constants');
 const collections = require('*/cartridge/scripts/util/collections');
 
 function processPayment(order, handlePaymentResult, req, res, emit) {
-    res.json({
-        error: false,
-        adyenAction: handlePaymentResult.action,
-        orderID: order.orderNo,
-        orderToken: order.orderToken,
-    });
-    emit('route:Complete');
+  res.json({
+    error: false,
+    adyenAction: handlePaymentResult.action,
+    orderID: order.orderNo,
+    orderToken: order.orderToken,
+  });
+  emit('route:Complete');
 }
 
 function isAdyen(currentBasket, next) {
-    let isAdyenBool = false;
+  let isAdyenBool = false;
 
-    collections.forEach(currentBasket.getPaymentInstruments(), function (paymentInstrument) {
-        if (
-            [
-                constants.METHOD_ADYEN,
-                paymentInstrument.METHOD_CREDIT_CARD,
-                constants.METHOD_ADYEN_POS,
-                constants.METHOD_ADYEN_COMPONENT,
-            ].indexOf(paymentInstrument.paymentMethod) !== -1
-        ) {
+  collections.forEach(
+    currentBasket.getPaymentInstruments(),
+    (paymentInstrument) => {
+      if (
+        [
+          constants.METHOD_ADYEN,
+          paymentInstrument.METHOD_CREDIT_CARD,
+          constants.METHOD_ADYEN_POS,
+          constants.METHOD_ADYEN_COMPONENT,
+        ].indexOf(paymentInstrument.paymentMethod) !== -1
+      ) {
+        isAdyenBool = true;
+      }
+    },
+  );
 
-            isAdyenBool = true;
-        }
-    });
-
-    if (!isAdyenBool) {
-        return next();
-    }
+  if (!isAdyenBool) {
+    return next();
+  }
+  return false;
 }
 
 module.exports = {
-    processPayment,
-    isAdyen,
+  processPayment,
+  isAdyen,
 };
