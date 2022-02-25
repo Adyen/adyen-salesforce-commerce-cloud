c0064d74745f53a3ed019470317923fea337196c
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2022-02-25 08:12:33 +0100

    Pw 6265 e2e sfra6 (#629)
    * introduce new structure to test SFRAv6
    
    * update github workflow
    
    * rename Class CheckoutPageSFRA to CheckoutPageSFRA5
    
    * remove fail-fast behavior from E2E workflow
    
    * remove shopperEmail from goToCheckoutPageWithFullCart. add email input to setShopperDetails
    
    * Attempt to enable concurrency
    
    * Reduce concurrent runners to 3
    
    Co-authored-by: Cenk Kucukiravul <cenkiravul35@gmail.com>

--- tests/e2e/data/environments.js
+++ tests/e2e/data/environments.js
@@ -1,22 +1,37 @@
-import CheckoutPageSFRA from "../pages/CheckoutPageSFRA";
+import CheckoutPageSFRA5 from "../pages/CheckoutPageSFRA5";
+import CheckoutPageSFRA6 from "../pages/CheckoutPageSFRA6";
 import CheckoutPageSG from "../pages/CheckoutPageSG";
 import AccountPageSFRA from "../pages/AccountPageSFRA";
 import AccountPageSG from "../pages/AccountPageSG";
 
-const environments = [
-  {
-    name: 'SFRA',
-    CheckoutPage: CheckoutPageSFRA,
-    urlExtension: '/s/RefArch/home',
-    AccountPage: AccountPageSFRA,
-  },
-  {
-    name: 'SG',
-    CheckoutPage: CheckoutPageSG,
-    urlExtension: '/on/demandware.store/Sites-SiteGenesis-Site',
-    AccountPage: AccountPageSG,
-  }
-]
+const environments = [];
+if(process.env.SFRA_VERSION === 'v5.3.0') {
+  environments.push(
+    {
+      name: 'SFRA v5.3.0',
+      CheckoutPage: CheckoutPageSFRA5,
+      urlExtension: '/s/RefArch/home',
+      AccountPage: AccountPageSFRA,
+    },
+    {
+      name: 'SG',
+      CheckoutPage: CheckoutPageSG,
+      urlExtension: '/on/demandware.store/Sites-SiteGenesis-Site',
+      AccountPage: AccountPageSG,
+    }
+  );
+}
+
+if(process.env.SFRA_VERSION === 'v6.0.0') {
+  environments.push(
+      {
+        name: 'SFRA v6.0.0',
+        CheckoutPage: CheckoutPageSFRA6,
+        urlExtension: '/s/RefArch/home',
+        AccountPage: AccountPageSFRA,
+      }
+  );
+}
 
 module.exports = {
   environments,
