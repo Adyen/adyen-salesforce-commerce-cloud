461b29dc1b5f9d02b08570fe1ec2adb6579ea29a
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2022-05-06 11:14:50 +0200

    Update SFRA to v6.1.0 from v6.0.0 in E2E workflow (#659)
    * Update SFRA to v6.1.0 from v6.0.0 in E2E workflow
    
    * fix constants and environment selection in AdyenHelper

--- tests/e2e/data/environments.js
+++ tests/e2e/data/environments.js
@@ -1,11 +1,11 @@
-import CheckoutPageSFRA5 from "../pages/CheckoutPageSFRA5";
-import CheckoutPageSFRA6 from "../pages/CheckoutPageSFRA6";
-import CheckoutPageSG from "../pages/CheckoutPageSG";
-import AccountPageSFRA from "../pages/AccountPageSFRA";
-import AccountPageSG from "../pages/AccountPageSG";
+import CheckoutPageSFRA5 from '../pages/CheckoutPageSFRA5';
+import CheckoutPageSFRA6 from '../pages/CheckoutPageSFRA6';
+import CheckoutPageSG from '../pages/CheckoutPageSG';
+import AccountPageSFRA from '../pages/AccountPageSFRA';
+import AccountPageSG from '../pages/AccountPageSG';
 
 const environments = [];
-if(process.env.SFRA_VERSION === 'v5.3.0') {
+if (process.env.SFRA_VERSION === 'v5.3.0') {
   environments.push(
     {
       name: 'SFRA v5.3.0',
@@ -18,21 +18,19 @@ if(process.env.SFRA_VERSION === 'v5.3.0') {
       CheckoutPage: CheckoutPageSG,
       urlExtension: '/on/demandware.store/Sites-SiteGenesis-Site',
       AccountPage: AccountPageSG,
-    }
+    },
   );
 }
 
-if(process.env.SFRA_VERSION === 'v6.0.0') {
-  environments.push(
-      {
-        name: 'SFRA v6.0.0',
-        CheckoutPage: CheckoutPageSFRA6,
-        urlExtension: '/s/RefArch/home',
-        AccountPage: AccountPageSFRA,
-      }
-  );
+if (process.env.SFRA_VERSION === 'v6.1.0') {
+  environments.push({
+    name: 'SFRA v6.1.0',
+    CheckoutPage: CheckoutPageSFRA6,
+    urlExtension: '/s/RefArch/home',
+    AccountPage: AccountPageSFRA,
+  });
 }
 
 module.exports = {
   environments,
-}
+};
