7e6a3891a6343254474693d06c6846e05f535f83
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2022-05-06 12:37:09 +0200

    removed auto generated core, updated package.json and babel

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,6 @@ module.exports = function (api) {
       ["@babel/plugin-proposal-decorators", { legacy: true }],
       "@babel/plugin-proposal-class-properties",
     ],
-    exclude: ["node_modules/**", "adyen_controllers_changes"],
+    only: ["./src/cartridges/bm_adyen", "./src/cartridges/int_adyen_controllers", "./src/cartridges/int_adyen_overlay", "./src/cartridges/int_adyen_SFRA"],
   };
 };
