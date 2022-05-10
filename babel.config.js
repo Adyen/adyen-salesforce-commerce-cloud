8f6ca9b0fedb20b940c9496b5ba5952866645c6a
zaiddreakh <zaid.dreakh@hotmail.co.uk>
2022-05-10 10:06:50 +0200

    changed babel config

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,6 @@ module.exports = function (api) {
       ["@babel/plugin-proposal-decorators", { legacy: true }],
       "@babel/plugin-proposal-class-properties",
     ],
-    only: ["./src/cartridges/bm_adyen", "./src/cartridges/int_adyen_controllers", "./src/cartridges/int_adyen_overlay", "./src/cartridges/int_adyen_SFRA"],
+    exclude: ["node_modules/**", "src/cartridges/adyen_controllers_changes"],
   };
 };
