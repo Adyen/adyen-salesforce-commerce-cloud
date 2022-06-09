b842b412ef58134280cc7b7d4796732b42f5c57c
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-06-09 14:58:30 +0200

    Fix old imports

--- tests/playwright/paymentFlows/presentToShopper.mjs
+++ tests/playwright/paymentFlows/presentToShopper.mjs
@@ -1,16 +1,11 @@
 import PaymentMethodsPage from '../pages/PaymentMethodsPage.mjs';
 const paymentMethodsPage = new PaymentMethodsPage(page);
 
-const doMultiBancoPayment = async () => {
+export const doMultiBancoPayment = async () => {
   await paymentMethodsPage.initiateMultiBancoPayment();
   return await paymentMethodsPage.MultiBancoVoucherExists();
 };
 
-const doBoletoPayment = async () => {
+export const doBoletoPayment = async () => {
   await paymentMethodsPage.initiateBoletoPayment();
-};
-
-module.exports = {
-  doMultiBancoPayment,
-  doBoletoPayment,
 };
