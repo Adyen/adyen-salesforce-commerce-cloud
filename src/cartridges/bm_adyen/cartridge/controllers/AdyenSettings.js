ccf85d207fba8cea2fcc4c754972c020bcefcd76
wouterboe <wouter.boereboom@adyen.com>
2022-06-13 16:11:21 +0200

    remove logging lines

--- src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
+++ src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
@@ -1,5 +1,4 @@
 const server = require('server');
-const Logger = require('dw/system/Logger');
 const Transaction = require('dw/system/Transaction');
 const csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 const AdyenConfigs = require('*/cartridge/scripts/util/adyenConfigs');
@@ -11,11 +10,10 @@ server.get('Start', csrfProtection.generateToken, (_req, res, next) => {
 
 server.post('Save', csrfProtection.generateToken, (req, res, next) => {
   const requestBody = JSON.parse(req.body);
-  Logger.getLogger('Adyen').error(typeof requestBody.adyenSFRA6Compatibility);
-  Logger.getLogger('Adyen').error(requestBody.adyenSFRA6Compatibility);
+
   const sfra6CompatibilityBoolean =
     requestBody.adyenSFRA6Compatibility === 'true';
-  Logger.getLogger('Adyen').error(sfra6CompatibilityBoolean);
+
   Transaction.begin();
   AdyenConfigs.setAdyenSFRA6Compatibility(sfra6CompatibilityBoolean);
   Transaction.commit();
