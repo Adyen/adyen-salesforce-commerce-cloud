a0813a57cc678fe9908e26fb99439ab4be82c987
wouterboe <wouter.boereboom@adyen.com>
2022-08-02 15:29:38 +0200

    create generic preference storage endpoint for BM extension

--- src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
+++ src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
@@ -11,12 +11,11 @@ server.get('Start', csrfProtection.generateToken, (_req, res, next) => {
 server.post('Save', csrfProtection.generateToken, (req, res, next) => {
   const requestBody = JSON.parse(req.body);
 
-  const sfra6CompatibilityBoolean =
-    requestBody.adyenSFRA6Compatibility === 'true';
-
-  Transaction.begin();
-  AdyenConfigs.setAdyenSFRA6Compatibility(sfra6CompatibilityBoolean);
-  Transaction.commit();
+  Object.entries(requestBody).forEach((key, value) => {
+    Transaction.wrap(() => {
+      AdyenConfigs.setCustomPreference(key, value);
+    });
+  });
   return next();
 });
 
