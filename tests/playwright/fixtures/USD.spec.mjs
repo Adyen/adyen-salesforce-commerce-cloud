e1669079a7acbe529dfd89f755f5a19de6d78b2a
cenkk <cenk.kucukiravul@adyen.com>
2022-08-11 15:25:21 +0200

    Do not set email for SFRA6 during PayPal checkout

--- tests/playwright/fixtures/USD.spec.mjs
+++ tests/playwright/fixtures/USD.spec.mjs
@@ -125,7 +125,11 @@ for (const environment of environments) {
 
     test('PayPal Success', async ({ page }) => {
       redirectShopper = new RedirectShopper(page);
-      await checkoutPage.setEmail();
+
+      // SFRA 6 email setting flow is different
+      if (environment.name.indexOf("v6") === -1) {
+        await checkoutPage.setEmail();
+      }
       await redirectShopper.doPayPalPayment();
       await checkoutPage.expectSuccess();
     });
