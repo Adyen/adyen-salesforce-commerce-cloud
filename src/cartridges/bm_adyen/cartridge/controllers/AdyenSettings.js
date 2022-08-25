83d8b789350f155a2fcf09a9fd1c285b6804ac15
wouterboe <wouter.boereboom@adyen.com>
2022-08-25 16:50:37 +0200

    include settingsscreen  front end functionalit & update. CSS and styling

--- src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
+++ src/cartridges/bm_adyen/cartridge/controllers/AdyenSettings.js
@@ -2,20 +2,32 @@ const server = require('server');
 const Transaction = require('dw/system/Transaction');
 const csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 const AdyenConfigs = require('*/cartridge/scripts/util/adyenConfigs');
+const Logger = require('dw/system/Logger');
 
 server.get('Start', csrfProtection.generateToken, (_req, res, next) => {
   res.render('adyenSettings/settings');
   return next();
 });
 
-server.post('Save', csrfProtection.generateToken, (req, res, next) => {
+server.post('Save', server.middleware.https, (req, res, next) => {
   const requestBody = JSON.parse(req.body);
 
-  requestBody.settings.forEach((setting) => {
-    Transaction.wrap(() => {
-      AdyenConfigs.setCustomPreference(setting.key, setting.value);
+  try{
+    requestBody.settings.forEach((setting) => {
+      Transaction.wrap(() => {
+        AdyenConfigs.setCustomPreference(setting.key, setting.value);
+      });
     });
-  });
+    res.json({
+      success: true,
+    })
+  } catch(error) {
+    Logger.getLogger('Adyen').error(`Error while saving settings in BM configuration: ${error}`);
+    res.json({
+      success: false,
+    })
+  }
+  
   return next();
 });
 
