88e42722eef48d9b808e637de2cc4f90bdd7d568
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2022-09-28 13:12:13 +0200

    Update adyenSettings.js

--- src/cartridges/bm_adyen/cartridge/static/default/adyenSettings.js
+++ src/cartridges/bm_adyen/cartridge/static/default/adyenSettings.js
@@ -4,140 +4,129 @@ document.addEventListener('DOMContentLoaded', () => {
   const cancelButton = document.querySelector('#settingsFormCancelButton');
   const formButtons = Array.from(document.getElementsByClassName('formButton'));
   const testConnectionButton = document.querySelector('#testConnectionButton');
-  const togglePassword = document.querySelector("#togglePassword");
-  const toggleApi = document.querySelector("#toggleApi");
+  const togglePassword = document.querySelector('#togglePassword');
+  const toggleApi = document.querySelector('#toggleApi');
   const formBody = document.querySelector('#formBody');
-  const password = document.querySelector("#notificationsPassword");
-  const merchAccount = document.getElementById("merchantAccount");
+  const password = document.querySelector('#notificationsPassword');
+  const merchAccount = document.getElementById('merchantAccount');
   const apiKeyVal = document.getElementById('apiKey');
   const changedSettings = [];
   testConnectionButton.disabled = true;
 
   function settingChanged(key, value) {
-    const settingIndex = changedSettings.findIndex(setting => {
-      return setting.key === key;
-    });
+    const settingIndex = changedSettings.findIndex(
+      (setting) => setting.key === key,
+    );
 
     if (settingIndex >= 0) {
       changedSettings[settingIndex] = {
         key,
-        value
+        value,
       };
     } else {
       changedSettings.push({
         key,
-        value
+        value,
       });
     }
   }
 
   function enableformButtons() {
-    for (const button of formButtons) {
+    formButtons.forEach((button) => {
       button.classList.remove('disabled');
       button.classList.add('enabled');
       form.removeEventListener('input', enableformButtons);
-    }
-
-    ;
+    });
   }
 
   function disableFormButtons() {
-    for (const button of formButtons) {
+    formButtons.forEach((button) => {
       button.classList.remove('enabled');
       button.classList.add('disabled');
       form.removeEventListener('input', enableformButtons);
-    }
-  } // add event for save button availability on form change.
+    });
+  }
 
-
-  function clickAndHide() {
+  function hideAlertsOnTest() {
     document.getElementById('settingsFormSubmitButton').click();
     document.getElementById('saveChangesAlert').hide();
     document.getElementById('notSavedChangesAlert').hide();
     document.getElementById('testConnectionButton').disabled = true;
   }
 
-  function showAlerts() {
+  function showAlertsOnSave() {
     document.getElementById('saveChangesAlert').show();
     document.getElementById('notSavedChangesAlert').show();
   }
 
-  function renderSafari(){
-    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)){
+  //if browser is safari and sets custom padding
+  function checkBrowserSupport() {
+    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
       formBody.style.setProperty('padding-top', '3rem');
     }
   }
 
-  testConnectionButton.addEventListener('click', clickAndHide); // add event listener to hide the alerts in case of test connection
+  testConnectionButton.addEventListener('click', hideAlertsOnTest); 
 
-  form.addEventListener('input', enableformButtons); // add event listener to maintain form updates
+  form.addEventListener('input', enableformButtons); 
 
-  submitButton.addEventListener('click', showAlerts); // add event listener to show alerts just in case of save button clicked
+  submitButton.addEventListener('click', showAlertsOnSave); 
 
-  window.addEventListener('load',renderSafari); // add event listerner to properly render css in case browser is safari 
+  window.addEventListener('load', checkBrowserSupport); 
 
-  form.addEventListener('change', event => {
-    const name = event.target.name;
-    let value = event.target.value; // get checked boolean value for checkboxes
+  // add event listener to maintain form updates
+  form.addEventListener('change', (event) => {
+    const { target } = event;
+    const { name } = target;
 
-    if (event.target.type === 'checkbox') {
-      value = event.target.checked;
-    } //convert radio button strings to boolean if values are 'true' or 'false'
+    // get checked boolean value for checkboxes and radio buttons
+    const isCheckedType = ['checkbox', 'radio'].some(
+      (type) => type === target.type,
+    );
 
-
-    if (event.target.type === 'radio') {
-      if (event.target.value === 'true') {
-        value = true;
-      }
-
-      if (event.target.value === 'false') {
-        value = false;
-      }
-    }
+    const value = isCheckedType ? target.checked : target.value;
 
     settingChanged(name, value);
-  }); 
-  
+  });
+
   // add event listener to test connection based on current form contents
   testConnectionButton.addEventListener('click', async () => {
     const response = await fetch('AdyenSettings-TestConnection', {
       headers: {
-        'Content-Type': 'application/json; charset=utf-8'
+        'Content-Type': 'application/json; charset=utf-8',
       },
       method: 'POST',
       body: JSON.stringify({
         xApiKey: document.getElementById('apiKey').value,
-        merchantAccount: document.getElementById('merchantAccount').value
-      })
+        merchantAccount: document.getElementById('merchantAccount').value,
+      }),
     });
     const data = await response.json();
 
     if (data.success) {
-      merchAccount.classList.add("is-valid");
+      merchAccount.classList.add('is-valid');
       merchAccount.classList.remove('is-invalid');
-      apiKeyVal.classList.add("is-valid");
+      apiKeyVal.classList.add('is-valid');
       apiKeyVal.classList.remove('is-invalid');
     } else {
-      merchAccount.classList.add("is-invalid");
-      merchAccount.classList.remove("is-valid");
-      apiKeyVal.classList.add("is-invalid");
-      apiKeyVal.classList.remove("is-valid");
+      merchAccount.classList.add('is-invalid');
+      merchAccount.classList.remove('is-valid');
+      apiKeyVal.classList.add('is-invalid');
+      apiKeyVal.classList.remove('is-valid');
     }
   });
-  
-  // add event to submit button to send form and present results
   submitButton.addEventListener('click', async () => {
     // disable form buttons and reattach event listener for enabling it on form change
     disableFormButtons();
     form.addEventListener('input', enableformButtons);
     const response = await fetch('AdyenSettings-Save', {
       headers: {
-        'Content-Type': 'application/json; charset=utf-8'
+        'Content-Type': 'application/json; charset=utf-8',
       },
       method: 'POST',
       body: JSON.stringify({
-        settings: changedSettings
-      })
+        settings: changedSettings,
+      }),
     });
     const data = await response.json();
 
@@ -155,20 +144,23 @@ document.addEventListener('DOMContentLoaded', () => {
       }, 2000);
     }
   });
+  
   cancelButton.addEventListener('click', async () => {
     location.reload();
-  }); 
-
-  togglePassword.addEventListener("click", function () {
-    const type = password.getAttribute("type") === "password" ? "text" : "password";
-    password.setAttribute("type", type);
-    this.classList.toggle("bi-eye");
   });
-  
-  toggleApi.addEventListener("click", function () {
-    const type = apiKeyVal.getAttribute("type") === "password" ? "text" : "password";
-    apiKeyVal.setAttribute("type", type);
-    this.classList.toggle("bi-eye");
+
+  togglePassword.addEventListener('click', function () {
+    const type =
+      password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+  });
+
+  toggleApi.addEventListener('click', function () {
+    const type =
+      apiKeyVal.getAttribute('type') === 'password' ? 'text' : 'password';
+    apiKeyVal.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
   });
 
   function openDialogCharityBackgroundUrl() {
@@ -179,8 +171,12 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('adyenGivingLogoUrl').click();
   }
 
-  document.getElementById('fileDropBoxCharitybackground').addEventListener('click', openDialogCharityBackgroundUrl);
-  document.getElementById('fileDropBoxGivingLogo').addEventListener('click', openDialogAdyenGivingLogoUrl);
+  document
+    .getElementById('fileDropBoxCharitybackground')
+    .addEventListener('click', openDialogCharityBackgroundUrl);
+  document
+    .getElementById('fileDropBoxGivingLogo')
+    .addEventListener('click', openDialogAdyenGivingLogoUrl);
 
   document.getElementById('flexSwitchCheckChecked').onchange = function () {
     document.getElementById('charityName').disabled = !this.checked;
