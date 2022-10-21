41d072fe38676653ed197edb97d88eabeab97bbf
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2022-10-21 08:14:28 +0200

    add klarna merchantdata to payment request (#696)
    * add klarna merchantdata to payment request
    
    * add extra comment
    
    add extra comment on base64 encoding if merchantData
    
    * add defensive coding and reduce duplication based on review comments

--- src/cartridges/int_adyen_overlay/cartridge/scripts/adyenCheckout.js
+++ src/cartridges/int_adyen_overlay/cartridge/scripts/adyenCheckout.js
@@ -27,6 +27,7 @@ const Logger = require('dw/system/Logger');
 const Resource = require('dw/web/Resource');
 const Order = require('dw/order/Order');
 const Transaction = require('dw/system/Transaction');
+const StringUtils = require('dw/util/StringUtils');
 
 /* Script Modules */
 const AdyenHelper = require('*/cartridge/scripts/util/adyenHelper');
@@ -112,6 +113,20 @@ function createPaymentRequest(args) {
       args.addTaxPercentage = true;
       if(paymentRequest.paymentMethod.type.indexOf('klarna') > -1){
         args.addTaxPercentage = false;
+        const address = order.getBillingAddress();
+        const shippingMethod = order.getDefaultShipment()?.shippingMethod;
+        const otherDeliveryAddress = {
+          shipping_method: shippingMethod?.displayName,
+          shipping_type: shippingMethod?.description,
+          first_name: address.firstName,
+          last_name: address.lastName,
+          street_address: `${address.address1} ${address.address2}`,
+          postal_code: address.postalCode,
+          city: address.city,
+          country: address.countryCode.value,
+        }
+        // openinvoicedata.merchantData holds merchant data. It takes data in a Base64 encoded string.
+        paymentRequest.additionalData['openinvoicedata.merchantData'] = StringUtils.encodeBase64(JSON.stringify(otherDeliveryAddress));
       }
       paymentRequest.lineItems = AdyenGetOpenInvoiceData.getLineItems(args);
       if (
