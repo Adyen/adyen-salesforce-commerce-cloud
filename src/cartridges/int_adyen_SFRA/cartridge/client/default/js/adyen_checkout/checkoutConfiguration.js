03bfb90a8773a3a598934d43006ae5bfd548e24e
Parallels <zenit_shkreli@hotmail.it>
2022-12-16 16:57:18 +0100

    displaying multiple gift card brands in confirmation page

--- src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyen_checkout/checkoutConfiguration.js
+++ src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyen_checkout/checkoutConfiguration.js
@@ -116,11 +116,16 @@ function handlePartialPaymentSuccess() {
 
 function getGiftCardConfig() {
   let giftcardBalance;
+  let brandArray = [];
   return {
     showPayButton: true,
     onChange: (state) => {
       store.updateSelectedPayment(constants.GIFTCARD, 'isValid', state.isValid);
       store.updateSelectedPayment(constants.GIFTCARD, 'stateData', state.data);
+      const brandSelect = document.getElementById('giftCardSelect');
+      const selectedBrandIndex = brandSelect.selectedIndex;
+      const giftcardBrand = brandSelect.options[selectedBrandIndex].text;
+      brandArray.push(giftcardBrand);
     },
     onBalanceCheck: (resolve, reject, requestData) => {
       $.ajax({
@@ -185,9 +190,10 @@ function getGiftCardConfig() {
         },
       });
     },
-    onSubmit(state, component) {
+    onSubmit(state) {
       store.selectedMethod = state.data.paymentMethod.type;
-      store.brand = component?.displayName;
+      store.brand = brandArray?.filter((item, i, ar) => ar.indexOf(item) === i)
+      .join(', ')      
       document.querySelector('input[name="brandCode"]').checked = false;
       document.querySelector('button[value="submit-payment"]').click();
     },
