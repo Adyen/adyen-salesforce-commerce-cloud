e86ae91560895de37cf3caed872f8dd892929971
Parallels <zenit_shkreli@hotmail.it>
2022-12-23 15:45:11 +0100

    lowercased log categories, so they can be recognized by dwease extension

--- src/cartridges/int_adyen_overlay/cartridge/scripts/adyenCustomLogs.js
+++ src/cartridges/int_adyen_overlay/cartridge/scripts/adyenCustomLogs.js
@@ -1,19 +1,19 @@
 var Logger = require('dw/system/Logger');
 
 function fatal_log(msg) {
-  return Logger.getLogger('Adyen_Fatal', 'Adyen').fatal(msg);
+  return Logger.getLogger('Adyen_fatal', 'Adyen').fatal(msg);
 }
 
 function error_log(msg) {
-  return Logger.getLogger('Adyen_Error', 'Adyen').error(msg);
+  return Logger.getLogger('Adyen_error', 'Adyen').error(msg);
 }
 
 function debug_log(msg) {
-  return Logger.getLogger('Adyen_Debug', 'Adyen').debug(msg);
+  return Logger.getLogger('Adyen_debug', 'Adyen').debug(msg);
 }
 
 function info_log(msg) {
-  return Logger.getLogger('Adyen_Info', 'Adyen').info(msg);
+  return Logger.getLogger('Adyen_info', 'Adyen').info(msg);
 }
 
 module.exports = {
