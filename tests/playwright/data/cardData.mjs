1e72e0dcc4ecb622f7f04e71284208f68748430e
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2023-01-09 11:41:28 +0100

    changed 3ds2 test card

--- tests/playwright/data/cardData.mjs
+++ tests/playwright/data/cardData.mjs
@@ -13,7 +13,7 @@ export class CardData {
   };
   threeDs2 = {
     holderName: 'John Doe',
-    cardNumber: '5454545454545454',
+    cardNumber: '4917 6100 0000 0000',
     expirationDate: '0330',
     cvc: '737',
   };
