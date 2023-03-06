f905c211e1fb4b9ea43b279144613cf24eb045f5
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2023-03-06 16:18:42 +0100

    fix: use txt file variable value

--- check-build.sh
+++ check-build.sh
@@ -2,7 +2,7 @@
 
 TXT_FILE=$([ "$1" = "SFRA" ] && echo "output-sfra.txt" || echo "output-sg.txt")
 
-if grep -q "Successfully uploaded cartridge" $(TXT_FILE); then
+if grep -q "Successfully uploaded cartridge" $TXT_FILE; then
     echo "Build successful"
     exit 0
 else
