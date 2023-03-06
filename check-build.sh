7febe1e86fe56e644e0cfa163e15b42472faaf2c
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2023-03-06 16:07:03 +0100

    fix: check SG build result

--- check-build.sh
+++ check-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if grep -q "Successfully uploaded cartridge" output.txt; then
+TXT_FILE=$([ "$1" = "SFRA" ] && echo "output-sfra.txt" || echo "output-sg.txt")
+
+if grep -q "Successfully uploaded cartridge" TXT_FILE; then
     echo "Build successful"
     exit 0
 else
