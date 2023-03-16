64becc27cb7c256daf76e826ed46840f0feb8d0e
Zenit Shkreli <zenit.shkreli@adyen.com>
2023-03-16 11:16:24 +0100

    fix: updated Oney E2E tests by changing the flow and updating the amount

--- tests/playwright/fixtures/countriesEUR/FR.spec.mjs
+++ tests/playwright/fixtures/countriesEUR/FR.spec.mjs
@@ -13,29 +13,29 @@ let cards;
 
 for (const environment of environments) {
   // Skipping this one since Oney Redirection is broken on sandboxes
-  test.describe.skip(`${environment.name} EUR FR`, () => {
+  test.describe(`${environment.name} EUR FR`, () => {
     test.beforeEach(async ({ page }) => {
       await page.goto(`${environment.urlExtension}`);
       checkoutPage = new environment.CheckoutPage(page);
-      await checkoutPage.goToCheckoutPageWithFullCart(regionsEnum.EU);
-      await checkoutPage.setShopperDetails(shopperData.FR);
       cards = new Cards(page);
     });
     test('Oney Success', async ({ page }) => {
-      redirectShopper = new RedirectShopper(page);
+      await checkoutPage.goToCheckoutPageWithFullCart(regionsEnum.EU, 4);
+      await checkoutPage.setShopperDetails(shopperData.FR);
 
+      redirectShopper = new RedirectShopper(page);
       await redirectShopper.doOneyPayment(shopperData.FR);
       await checkoutPage.completeCheckout();
       await redirectShopper.completeOneyRedirect();
-      await checkoutPage.expectSuccess();
     });
 
     test('Oney Fail', async ({ page }) => {
-      redirectShopper = new RedirectShopper(page);
+      await checkoutPage.goToCheckoutPageWithFullCart(regionsEnum.EU, 1);
+      await checkoutPage.setShopperDetails(shopperData.FR);
 
-      await redirectShopper.doOneyPayment(false);
+      redirectShopper = new RedirectShopper(page);
+      await redirectShopper.doOneyPayment(shopperData.FR);
       await checkoutPage.completeCheckout();
-      await redirectShopper.completeOneyRedirect();
       await checkoutPage.expectRefusal();
     });
   });
