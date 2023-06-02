8287e3cd73740d0503986b5e0783e63d848afa81
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2023-06-02 09:58:55 +0200

    chore: removed built folder

--- cartridges/int_adyen_SFRA/cartridge/scripts/checkout/utils/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var getPayments = require('*/cartridge/scripts/checkout/utils/getPayments');
-var validatePaymentMethod = require('*/cartridge/scripts/checkout/utils/validatePaymentMethod');
-module.exports = {
-  getPayments: getPayments,
-  validatePaymentMethod: validatePaymentMethod
-};
