22dd43e01af1dfeb13dbdb1e849b85baf0a067bc
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2023-08-14 14:41:13 +0200

    Adyen Giving description and name fields (#951)

--- src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyenGiving.js
+++ src/cartridges/int_adyen_SFRA/cartridge/client/default/js/adyenGiving.js
@@ -40,9 +40,9 @@ function getAmounts() {
 const donationConfig = {
   amounts: getAmounts(),
   backgroundUrl: adyenGivingBackgroundUrl,
-  description: charityDescription,
+  description: decodeURI(charityDescription),
   logoUrl: adyenGivingLogoUrl,
-  name: charityName,
+  name: decodeURI(charityName),
   url: charityWebsite,
   showCancelButton: true,
   onDonate: handleOnDonate,
