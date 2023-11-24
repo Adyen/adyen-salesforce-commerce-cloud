3428911a3c13381a8792e7f9f42c8dd241443341
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2023-11-24 16:35:45 +0100

    fix: reduced number of runners for more stable testing

--- tests/playwright/sfcc.config.js
+++ tests/playwright/sfcc.config.js
@@ -27,7 +27,7 @@ const config = {
   retries: process.env.CI ? 1 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 5 : 1,
+  workers: process.env.CI ? 3 : 1,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
