0795ef6e808b9326d2830eda7cd2aecbb206aecf
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2023-12-28 10:28:21 +0100

    [SFI-260] Increased unit test coverage (#1030)
    * test: increased unit test coverage
    
    * chore: skipping one test + linting
    
    * chore: skipping makepartialpayments tests
    
    * fix: refactored makePartialPayment function and fixed the corresponding tests
    
    * fix: fixed also the unit tests to comply the error messages
    
    * fix: fixed adding mocks for github actions
    
    * fix: awaiting the promise to be resolved to remove flakiness in e2e

---
+++ src/cartridges/int_adyen_SFRA/cartridge/controllers/middlewares/adyen/__tests__/selectShippingMethods.test.js
@@ -0,0 +1,90 @@
+/* eslint-disable global-require */
+const BasketMgr = require('dw/order/BasketMgr');
+const shippingHelper = require('*/cartridge/scripts/checkout/shippingHelpers');
+const URLUtils = require('dw/web/URLUtils');
+let req;
+let res;
+let next;
+let currentBasket;
+const callSelectShippingMethod = require('../selectShippingMethods');
+
+describe('callSelectShippingMethod', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    req = {
+      querystring: {
+      },
+      form: {
+      },
+    };
+
+    res = {
+      json: jest.fn(),
+      setStatusCode: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  it('should handle the case when there is no current basket', () => {
+    currentBasket = BasketMgr.getCurrentBasket.mockReturnValueOnce(null);
+    callSelectShippingMethod(req, res, next);
+    expect(res.json).toHaveBeenCalledWith({
+        error: true,
+        redirectUrl: URLUtils.url('Cart-Show').toString(),
+      });
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should handle the case when there is an error selecting the shipping method', () => {
+    req.querystring.shipmentUUID = 'mocked_uuid';
+    currentBasket = {
+      defaultShipment: {},
+    };
+    BasketMgr.getCurrentBasket.mockReturnValueOnce(currentBasket.defaultShipment);
+    shippingHelper.getShipmentByUUID.mockReturnValueOnce(currentBasket.defaultShipment);
+  
+    callSelectShippingMethod(req, res, next);
+
+    expect(res.setStatusCode).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+        errorMessage: 'mocked_error.cannot.select.shipping.method',
+      });
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should handle the case when there is no error', () => {
+    const currentBasket = {
+      defaultShipment: {
+        shippingMethod: {
+          description: 'Order received within 7-10 business days',
+          displayName: 'Ground',
+          ID: '001',
+          custom: {
+            estimatedArrivalTime: '7-10 Business Days',
+          },
+        },
+      },
+      getTotalGrossPrice: jest.fn(() => ({ value: 100, currencyCode: 'USD' })),
+    };
+    BasketMgr.getCurrentBasket.mockReturnValueOnce(currentBasket);
+    shippingHelper.selectShippingMethod.mockImplementationOnce(() => {});
+
+    const CartModel = require('*/cartridge/models/cart');
+    const basketModelInstance = {
+      someMethod: jest.fn(),
+    };
+    CartModel.mockReturnValueOnce(basketModelInstance);
+    
+    callSelectShippingMethod(req, res, next);  
+
+    expect(res.json).toHaveBeenCalledWith({
+      ...basketModelInstance,
+      grandTotalAmount: {
+        value: 100,
+        currency: 'USD',
+      },
+    });
+    expect(next).toHaveBeenCalled();
+  });
+});
