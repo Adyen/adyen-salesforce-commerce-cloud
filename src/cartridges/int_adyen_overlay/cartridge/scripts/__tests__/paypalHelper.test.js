c0e7a907734ad2b8a71ae60f18dbe8f8939f28e6
shani <shani.singh@adyen.com>
2024-04-19 13:17:53 +0200

    chore: fix eslint issues

--- src/cartridges/int_adyen_overlay/cartridge/scripts/__tests__/paypalHelper.test.js
+++ src/cartridges/int_adyen_overlay/cartridge/scripts/__tests__/paypalHelper.test.js
@@ -1,65 +1,43 @@
 const paypalHelper = require('../paypalHelper')
 describe('paypalHelper', () => {
-  it('should return lineItems for paypal', () => {
-const args = {
-  Order: {
-    getProductLineItems: jest.fn(() => {
-      return {
-        toArray: jest.fn(() => ([
-          {
-            productName: 'test',
-            productID: '123',
-            quantityValue: '1',
-            getAdjustedTax: '1000',
-            adjustedNetPrice: '10000',
-            category: 'PHYSICAL_GOODS',
+  let args,lineItem, result
+  beforeEach(() => {
+    args = (item) => ({
+      Order: {
+        getProductLineItems: jest.fn(() => {
+          return {
+            toArray: jest.fn(() => ([item]))
           }
-        ]))
+        })
       }
     })
-  }
-}
 
-const paypalLineItems = paypalHelper.getLineItems(args)
-    expect(paypalLineItems[0]).toStrictEqual({
+    lineItem = {
+      productName: 'test',
+      productID: '123',
+      quantityValue: '1',
+      getAdjustedTax: '1000',
+      adjustedNetPrice: '10000',
+      category: 'PHYSICAL_GOODS',
+    }
+
+    result = {
       quantity: '1',
       description: 'test',
       itemCategory: 'PHYSICAL_GOODS',
       sku: '123',
       amountExcludingTax: '10000',
       taxAmount: '1000'
-    })
+    }
+  })
+  it('should return lineItems for paypal', () => {
+const paypalLineItems = paypalHelper.getLineItems(args(lineItem))
+    expect(paypalLineItems[0]).toStrictEqual(result)
   })
 
   it('should return lineItems for paypal with default itemCategory when category is not as per paypal', () => {
-    const args = {
-      Order: {
-        getProductLineItems: jest.fn(() => {
-          return {
-            toArray: jest.fn(() => ([
-              {
-                productName: 'test',
-                productID: '123',
-                quantityValue: '2',
-                getAdjustedTax: '1000',
-                adjustedNetPrice: '10000',
-                category: 'TEST_GOODS',
-              }
-            ]))
-          }
-        })
-      }
-    }
-
-    const paypalLineItems = paypalHelper.getLineItems(args)
-    expect(paypalLineItems[0]).toStrictEqual({
-      quantity: '2',
-      description: 'test',
-      itemCategory: 'PHYSICAL_GOODS',
-      sku: '123',
-      amountExcludingTax: '5000',
-      taxAmount: '500'
-    })
+    const paypalLineItems = paypalHelper.getLineItems(args({...lineItem, category: 'TEST_GOODS'}))
+    expect(paypalLineItems[0]).toStrictEqual(result)
   })
 
   it('should return no lineItems for paypal if order or basket is not defined', () => {
