6641c6d3bb921c8d9385b8d6217d72321e31cf5d
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2024-06-11 12:12:43 +0200

    Added a check to set address properties in case they are available (#1093)
    * fix: added a check to set address properties in case they are available
    
    * chore: adding function description

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/scripts/expressPayments/shippingMethods.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/scripts/expressPayments/shippingMethods.js
@@ -3,6 +3,27 @@ const Transaction = require('dw/system/Transaction');
 const AdyenLogs = require('*/cartridge/adyen/logs/adyenCustomLogs');
 const AdyenHelper = require('*/cartridge/adyen/utils/adyenHelper');
 
+const addressMapping = {
+  city: 'setCity',
+  countryCode: 'setCountryCode',
+  stateCode: 'setStateCode',
+  postalCode: 'setPostalCode',
+};
+
+/**
+ * Sets address properties for express PM
+ * @param {dw.order.shippingAddress} shippingAddress - shippingAddress for the default shipment
+ * @param {object} inputAddress - address coming from the input field based on shopper selection
+ * @param {object} mapping - address mapping between property and setter for that property
+ */
+function setAddressProperties(shippingAddress, inputAddress, mapping) {
+  Object.keys(inputAddress).forEach((key) => {
+    if (inputAddress[key] && mapping[key]) {
+      shippingAddress[mapping[key]](inputAddress[key]);
+    }
+  });
+}
+
 /**
  * Make a request to Adyen to get shipping methods
  */
@@ -26,10 +47,9 @@ function callGetShippingMethods(req, res, next) {
           .getDefaultShipment()
           .createShippingAddress();
       }
-      shippingAddress.setCity(address.city);
-      shippingAddress.setPostalCode(address.postalCode);
-      shippingAddress.setStateCode(address.stateCode);
-      shippingAddress.setCountryCode(address.countryCode);
+      if (address) {
+        setAddressProperties(shippingAddress, address, addressMapping);
+      }
     });
     const currentShippingMethodsModels =
       AdyenHelper.getApplicableShippingMethods(shipment, address);
