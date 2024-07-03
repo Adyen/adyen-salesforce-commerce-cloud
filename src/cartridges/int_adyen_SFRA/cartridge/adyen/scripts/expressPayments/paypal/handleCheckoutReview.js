3d691dcc07c807ff7c2bbefdfe8fb4c118f8a4eb
shani <shani.singh@adyen.com>
2024-07-03 22:57:07 +0200

    feat(SFI-789): handle payments details call from checkout review page

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/scripts/expressPayments/paypal/handleCheckoutReview.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/scripts/expressPayments/paypal/handleCheckoutReview.js
@@ -1,7 +1,22 @@
+const URLUtils = require('dw/web/URLUtils');
+const AdyenLogs = require('*/cartridge/adyen/logs/adyenCustomLogs');
+
 function handleCheckoutReview(req, res, next) {
-  res.render('cart/checkoutReview', {
-    // additional data may be required later
-  });
+  try {
+    if (!req.form.data) {
+      throw new Error('State data not present in the request');
+    }
+    res.render('cart/checkoutReview', {
+      data: req.form.data,
+      showConfirmationUrl: URLUtils.https(
+        'Adyen-ShowConfirmationPaymentFromComponent',
+      ),
+      // additional data may be required later
+    });
+  } catch (error) {
+    AdyenLogs.error_log('Could not render checkout review page', error);
+    res.redirect(URLUtils.url('Error-ErrorCode', 'err', 'general'));
+  }
   return next();
 }
 
