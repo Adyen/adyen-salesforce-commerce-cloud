44a2d3c3b41e4f5b6a26ba7038a9d4dec99f93db
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2024-07-09 13:16:41 +0200

    chore : linting

--- tests/playwright/fixtures/USD.spec.mjs
+++ tests/playwright/fixtures/USD.spec.mjs
@@ -264,12 +264,12 @@ for (const environment of environments) {
         redirectShopper = new RedirectShopper(page);
         await redirectShopper.doPayPalPayment(true, false, true);
         if (environment.name.indexOf('v5') !== -1) {
-			await page.locator("button[value='place-order']").click();
+	    await page.locator("button[value='place-order']").click();
             await page.locator(".order-thank-you-msg").isVisible({ timeout: 20000 });
         }
-		else{
+	else {
             await checkoutPage.expectSuccess();
-		}
+	}
     });
 
 	test('PayPal Express shipping change @quick', async ({ page }) => {
@@ -280,11 +280,11 @@ for (const environment of environments) {
         await redirectShopper.doPayPalPayment(true, true, true);
         if (environment.name.indexOf('v5') !== -1) {
             await page.locator("button[value='place-order']").click();
-			await page.locator(".order-thank-you-msg").isVisible({ timeout: 20000 });
+	    await page.locator(".order-thank-you-msg").isVisible({ timeout: 20000 });
         }
-		else{
-			await checkoutPage.expectSuccess();
-		}
+	else {
+	    await checkoutPage.expectSuccess();
+	}
     });
 
 	test('PayPal Express Cancellation @quick', async ({ page }) => {
