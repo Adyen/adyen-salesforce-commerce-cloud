d9bd396e35d95a99cfdad07ff942ceea83473334
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2024-10-04 10:41:32 +0200

    chore: replacing only with skip

--- tests/playwright/fixtures/countriesEUR/NL.spec.mjs
+++ tests/playwright/fixtures/countriesEUR/NL.spec.mjs
@@ -77,7 +77,7 @@ for (const environment of environments) {
       await checkoutPage.expectRefusal();
     });
 
-    test.only('SEPA Success @quick', async ({ page }) => {
+    test.skip('SEPA Success @quick', async ({ page }) => {
       pendingPayments = new PendingPayments(page);
       await pendingPayments.doSEPAPayment();
       await checkoutPage.completeCheckout();
