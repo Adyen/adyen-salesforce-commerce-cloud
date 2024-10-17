232f942816c26f271843e904f3727603b0fc3e52
shani <shani.singh@adyen.com>
2024-10-17 10:11:27 +0200

    chore(SFI-954): add mocks

--- jest/sfccPathSetup.js
+++ jest/sfccPathSetup.js
@@ -513,3 +513,10 @@ jest.mock(
     require('../src/cartridges/int_adyen_SFRA/cartridge/adyen/analytics/analyticsEvents'),
   { virtual: true },
 );
+
+jest.mock(
+  '*/cartridge/adyen/analytics/constants',
+  () =>
+    require('../src/cartridges/int_adyen_SFRA/cartridge/adyen/analytics/constants'),
+  { virtual: true },
+);
