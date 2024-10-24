edea7aefa20595ef2ddea55d7e58486f915bf9e7
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2024-10-24 11:03:53 +0200

    Job for processing the analytics events, and deleting them (#1195)

---
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/analytics/job/analyticsJob.js
@@ -0,0 +1,140 @@
+const CustomObjectMgr = require('dw/object/CustomObjectMgr');
+const Transaction = require('dw/system/Transaction');
+const constants = require('../constants');
+const AnalyticsService = require('../analyticsService');
+const AdyenLogs = require('../../logs/adyenCustomLogs');
+
+function createRequestObjectForAllReferenceIds(groupedObjects) {
+  const requestObject = {
+    channel: 'Web',
+    platform: 'Web',
+  };
+
+  // Iterate over all referenceIds and group events into one requestObject
+  Object.keys(groupedObjects).forEach((referenceId) => {
+    const events = groupedObjects[referenceId];
+
+    events.forEach((event) => {
+      const eventObject = {
+        timestamp: new Date(event.creationDate).getTime().toString(),
+        type: 'focus', // this has to be changed once API accepts our event types
+        target: event.eventStatus,
+        id: event.eventId,
+        component: event.eventSource,
+      };
+
+      const eventCode = event.eventCode.toLowerCase();
+      const eventTypes = [
+        constants.eventCode.INFO,
+        constants.eventCode.ERROR,
+        constants.eventCode.LOG,
+      ];
+
+      if (eventTypes.includes(eventCode)) {
+        if (!requestObject[eventCode]) {
+          requestObject[eventCode] = [];
+        }
+        requestObject[eventCode].push(eventObject);
+      }
+    });
+  });
+
+  return requestObject;
+}
+
+function deleteCustomObject(customObject) {
+  try {
+    Transaction.wrap(() => {
+      CustomObjectMgr.remove(customObject);
+    });
+  } catch (e) {
+    AdyenLogs.error_log('Error deleting custom object:', e);
+  }
+}
+
+function iterateCustomObjects(
+  customObjectIterator,
+  groupedObjects,
+  customObjectsToDelete,
+) {
+  while (customObjectIterator.hasNext()) {
+    const customObject = customObjectIterator.next();
+    const { referenceId } = customObject.custom;
+    const combinedFields = {};
+
+    if (!groupedObjects[referenceId]) {
+      groupedObjects[referenceId] = [];
+    }
+
+    // Extract custom fields
+    Object.keys(customObject.custom).forEach((fieldName) => {
+      combinedFields[fieldName] = customObject.custom[fieldName]?.toString();
+    });
+    combinedFields.creationDate = customObject.creationDate;
+    groupedObjects[referenceId].push(combinedFields);
+
+    customObjectsToDelete.push(customObject);
+  }
+}
+
+function updateProcessingStatus(customObject, status) {
+  try {
+    Transaction.wrap(() => {
+      customObject.custom.processingStatus = status;
+    });
+  } catch (e) {
+    AdyenLogs.error_log('Error updating processing status:', e);
+  }
+}
+
+function processData() {
+  const query = 'custom.processingStatus = {0}';
+  const queryArgs = [constants.processingStatus.NOT_PROCESSED];
+  let customObjectIterator = null;
+  const groupedObjects = {};
+
+  try {
+    // Query the custom objects by processing status
+    customObjectIterator = CustomObjectMgr.queryCustomObjects(
+      constants.analyticsEventObjectId,
+      query,
+      null,
+      queryArgs,
+    );
+    const customObjectsToDelete = [];
+
+    iterateCustomObjects(
+      customObjectIterator,
+      groupedObjects,
+      customObjectsToDelete,
+    );
+
+    const payload = createRequestObjectForAllReferenceIds(groupedObjects);
+    const submissionSuccess = AnalyticsService.submitData(payload);
+    if (submissionSuccess) {
+      customObjectsToDelete.forEach((customObject) => {
+        deleteCustomObject(customObject);
+      });
+    } else {
+      AdyenLogs.error_log('Failed to submit full payload for grouped objects.');
+      // This will be triggered upon completion of SFI-991
+      customObjectsToDelete.forEach((customObject) => {
+        updateProcessingStatus(
+          customObject,
+          constants.processingStatus.SKIPPED,
+        );
+      });
+    }
+  } catch (e) {
+    AdyenLogs.error_log(`Error querying custom objects: ${e}`);
+  } finally {
+    if (customObjectIterator) {
+      customObjectIterator.close();
+    }
+  }
+  return groupedObjects;
+}
+
+module.exports = {
+  processData,
+};
