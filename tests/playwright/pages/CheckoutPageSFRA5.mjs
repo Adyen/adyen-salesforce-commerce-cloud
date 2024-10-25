5d0b9aafb0d8e53539eda7800165edf430a3cdb6
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2024-10-25 08:41:16 +0200

    fix: adding a timeout to let component time to render c2p

--- tests/playwright/pages/CheckoutPageSFRA5.mjs
+++ tests/playwright/pages/CheckoutPageSFRA5.mjs
@@ -162,8 +162,8 @@ export default class CheckoutPageSFRA5 {
     await this.checkoutPageUserEmailInput.fill('');
     await this.checkoutPageUserEmailInput.fill(email);
     // Pressing Tab to simulate component re-rendering and waiting the components to re-mount
+	await new Promise(r => setTimeout(r, 2000));
     await this.page.keyboard.press('Tab');
-    await new Promise(r => setTimeout(r, 2000));
   };
 
   submitShipping = async () => {
