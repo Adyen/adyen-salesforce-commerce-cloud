a81bf7b6f8cec85d2832f0b02bceb21f08816fa0
aleksandarm <aleksandar.mihajlovski@adyen.com>
2025-02-13 20:44:09 +0100

    fix: stored card

--- src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen_checkout/paymentMethodsConfiguration/card/storedCardConfig.js
+++ src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen_checkout/paymentMethodsConfiguration/card/storedCardConfig.js
@@ -3,16 +3,16 @@ const CardConfig = require('./cardConfig');
 class StoredCardConfig extends CardConfig {
   constructor(store, helpers) {
     super(store, helpers);
-    this.holderNameRequired = true;
+    this.holderNameRequired = false;
   }
 
-  getConfig = () => {
+  getConfig() {
     const baseConfig = super.getConfig();
     return {
       ...baseConfig,
       holderNameRequired: this.holderNameRequired,
     };
-  };
+  }
 }
 
 module.exports = StoredCardConfig;
