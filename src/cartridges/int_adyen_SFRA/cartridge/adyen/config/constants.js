eba94cffcfee5ee2b987bd6e40fd02bb96d3b953
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2025-02-20 10:16:48 +0100

    New front-end region for APSE (#1265)
    * feat: added new FE region
    
    * feat: added new FE region in BM
    
    * chore: linting

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
@@ -79,6 +79,7 @@ module.exports = {
     AU: 'AU',
     EU: 'EU',
     IN: 'IN',
+    APSE: 'APSE',
   },
 
   SHOPPER_INTERACTIONS: {
@@ -105,6 +106,7 @@ module.exports = {
   CHECKOUT_ENVIRONMENT_LIVE_US: 'live-us',
   CHECKOUT_ENVIRONMENT_LIVE_AU: 'live-au',
   CHECKOUT_ENVIRONMENT_LIVE_IN: 'live-in',
+  CHECKOUT_ENVIRONMENT_LIVE_APSE: 'live-apse',
 
   MERCHANT_APPLICATION_NAME: 'adyen-salesforce-commerce-cloud',
   EXTERNAL_PLATFORM_NAME: 'SalesforceCommerceCloud',
