b5f64b02f61255926def1faaec6d6e08fcd43ccd
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2025-05-28 11:06:26 +0200

    fix: validate component (#1345)

--- src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen/checkout/index.js
+++ src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen/checkout/index.js
@@ -178,7 +178,7 @@ function submitPayment() {
     ) {
       assignPaymentMethodValue();
       setAdyenInputValues();
-      showValidation();
+      return showValidation();
     }
     return true;
   });
