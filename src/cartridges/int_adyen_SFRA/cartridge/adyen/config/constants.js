b4f08189892c25e48a828fd8b29f4a6454f28d23
aleksandarm <aleksandar.mihajlovski@adyen.com>
2025-06-17 09:48:03 +0200

    chore: upgrade component version

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
@@ -155,7 +155,7 @@ module.exports = {
   APPLE_DOMAIN_URL:
     '/.well-known/apple-developer-merchantid-domain-association',
 
-  CHECKOUT_COMPONENT_VERSION: '6.18g.0',
+  CHECKOUT_COMPONENT_VERSION: '6.18.0',
   CHECKOUT_API_VERSION: 'v71',
   VERSION: '25.3.0',
 };
