6194fa06a2823cba9dc5330ebc8924c5bacc991e
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2025-09-04 14:50:31 +0200

    [SFI-1365] Upgrade adyen web version (#1395)
    * feat: upgrade to latest adyen web component version
    
    * fix: upi e2e tests
    
    * fix: use continue button only for upi_collect

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
@@ -156,7 +156,7 @@ module.exports = {
   APPLE_DOMAIN_URL:
     '/.well-known/apple-developer-merchantid-domain-association',
 
-  CHECKOUT_COMPONENT_VERSION: '6.16.0',
+  CHECKOUT_COMPONENT_VERSION: '6.21.0',
   CHECKOUT_API_VERSION: 'v71',
   VERSION: '25.4.0',
 };
