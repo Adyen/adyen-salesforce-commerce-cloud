f16727ff58fd1f151786963171572717d0c3f2c9
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2025-09-25 15:05:33 +0200

    fix: improved and simplifies the job functions (#1413)

--- src/cartridges/int_adyen_webhooks/cartridge/job/notifications.js
+++ src/cartridges/int_adyen_webhooks/cartridge/job/notifications.js
@@ -23,15 +23,77 @@
 const OrderMgr = require('dw/order/OrderMgr');
 const Transaction = require('dw/system/Transaction');
 const CustomObjectMgr = require('dw/object/CustomObjectMgr');
-const COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
 const Locale = require('dw/util/Locale');
-//script includes
+const COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
+// script includes
 const AdyenLogs = require('*/cartridge/adyen/logs/adyenCustomLogs');
+const deleteCustomObjects = require('*/cartridge/deleteCustomObjects');
+const objectsHandler = require('*/cartridge/handleCustomObject');
 
-function execute() {
-  processNotifications();
-  clearNotifications();
-  return PIPELET_NEXT;
+/**
+ * Handles failed order scenarios
+ * @param {Object} handlerResult - The result from the handler
+ * @param {Object} order - The order object
+ * @returns {boolean} True if processing should continue, false if should skip
+ */
+function handleFailedOrder(handlerResult, order) {
+  if (!handlerResult.status || handlerResult.status === PIPELET_ERROR) {
+    // Only CREATED orders can be failed
+    if (!order || order.status.value !== dw.order.Order.ORDER_STATUS_CREATED) {
+      return false;
+    }
+    // Refused payments which are made with using Adyen payment method are
+    // handled when user is redirected back from Adyen HPP.
+    // Here we shouldn't fail an order and send a notification
+    Transaction.wrap(() => {
+      OrderMgr.failOrder(order, true);
+    });
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Handles successful order processing
+ * @param {Object} handlerResult - The result from the handler
+ * @param {Object} order - The order object
+ * @returns {boolean} True if processing should continue, false if should skip
+ */
+function handleSuccessfulOrder(handlerResult, order) {
+  if (handlerResult.SkipOrder || handlerResult.Pending) {
+    return false;
+  }
+
+  // Send confirmation email
+  if (handlerResult.SubmitOrder) {
+    const customerLocaleId = order.getCustomerLocaleID();
+    const customerLocale = Locale.getLocale(customerLocaleId);
+    COHelpers.sendConfirmationEmail(order, customerLocale);
+  }
+  return true;
+}
+
+/**
+ * Processes a single notification custom object
+ * @param {Object} customObj - The custom object to process
+ */
+function processNotificationItem(customObj) {
+  let handlerResult;
+  Transaction.wrap(() => {
+    handlerResult = objectsHandler.handle(customObj);
+  });
+
+  /*
+    Sometimes order cannot be found in DWRE DB even if it exists there,
+    in that case we shouldn't reply to Adyen that all was ok in order to get a new notification
+  */
+  const order = handlerResult.Order;
+
+  if (!handleFailedOrder(handlerResult, order)) {
+    return;
+  }
+
+  handleSuccessfulOrder(handlerResult, order);
 }
 
 /**
@@ -39,7 +101,6 @@ function execute() {
  *  to be processed and handle them to place or fail order
  */
 function processNotifications(/* pdict */) {
-  const objectsHandler = require('*/cartridge/handleCustomObject');
   const searchQuery = CustomObjectMgr.queryCustomObjects(
     'adyenNotification',
     "custom.updateStatus = 'PROCESS'",
@@ -49,48 +110,11 @@ function processNotifications(/* pdict */) {
     `Process notifications start with count ${searchQuery.count}`,
   );
 
-  let customObj;
-  let handlerResult;
-  let order;
   while (searchQuery.hasNext()) {
-    customObj = searchQuery.next();
-    Transaction.wrap(() => {
-      handlerResult = objectsHandler.handle(customObj);
-    });
-
-    /*
-      Sometimes order cannot be found in DWRE DB even if it exists there,
-      in that case we shouldn't reply to Adyen that all was ok in order to get a new notification
-    */
-    order = handlerResult.Order;
-    const customerLocaleId = order.getCustomerLocaleID();
-    const customerLocale = Locale.getLocale(customerLocaleId);
-    if (!handlerResult.status || handlerResult.status === PIPELET_ERROR) {
-      // Only CREATED orders can be failed
-      if (
-        !order ||
-        order.status.value !== dw.order.Order.ORDER_STATUS_CREATED
-      ) {
-        continue;
-      }
-      // Refused payments which are made with using Adyen payment method are
-      // handled when user is redirected back from Adyen HPP.
-      // Here we shouldn't fail an order and send a notification
-      Transaction.wrap(() => {
-        OrderMgr.failOrder(order, true);
-      });
-      continue;
-    }
-
-    if (handlerResult.SkipOrder || handlerResult.Pending) {
-      continue;
-    }
-
-    // Send confirmation email
-    if (handlerResult.SubmitOrder) {
-      COHelpers.sendConfirmationEmail(order, customerLocale);
-    }
+    const customObj = searchQuery.next();
+    processNotificationItem(customObj);
   }
+
   AdyenLogs.info_log(
     `Process notifications finished with count ${searchQuery.count}`,
   );
@@ -100,10 +124,19 @@ function processNotifications(/* pdict */) {
 }
 
 /**
+ * Removes a single processed custom object
+ * @param {Object} customObj - The custom object to remove
+ */
+function removeProcessedCustomObject(customObj) {
+  Transaction.wrap(() => {
+    deleteCustomObjects.remove(customObj);
+  });
+}
+
+/**
  * cleanNotifications
  */
 function clearNotifications(/* pdict */) {
-  const deleteCustomObjects = require('*/cartridge/deleteCustomObjects');
   const searchQuery = CustomObjectMgr.queryCustomObjects(
     'adyenNotification',
     "custom.processedStatus = 'SUCCESS'",
@@ -113,18 +146,21 @@ function clearNotifications(/* pdict */) {
     `Removing Processed Custom Objects start with count ${searchQuery.count}`,
   );
 
-  let customObj;
   while (searchQuery.hasNext()) {
-    customObj = searchQuery.next();
-    Transaction.wrap(() => {
-      deleteCustomObjects.remove(customObj);
-    });
+    const customObj = searchQuery.next();
+    removeProcessedCustomObject(customObj);
   }
   AdyenLogs.info_log(
     `Removing Processed Custom Objects finished with count ${searchQuery.count}`,
   );
   searchQuery.close();
 
+  return PIPELET_NEXT;
+}
+
+function execute() {
+  processNotifications();
+  clearNotifications();
   return PIPELET_NEXT;
 }
 
