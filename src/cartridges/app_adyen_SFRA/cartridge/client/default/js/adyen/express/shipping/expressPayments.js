16d686acd8e5860f1ee3b73ceaa374b45e07d6c3
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2025-12-10 15:08:36 +0100

    fix: destroying previous instances of paypal express component

--- src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen/express/shipping/expressPayments.js
+++ src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen/express/shipping/expressPayments.js
@@ -140,6 +140,12 @@ async function renderPayPalButtonListener(e, response) {
   if (!paypalConfig) return;
   const paypal = new Paypal(paypalConfig, applicationInfo, adyenTranslations);
   const paypalComponent = await paypal.getComponent();
+
+  // This is a hack to destroy the previous paypal component as PayPal does not support multiple components on the same page
+  if (window.paypal) {
+    window.paypal.__internal_destroy__();
+  }
+
   paypalComponent.mount(button);
 }
 
