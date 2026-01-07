4c892970f2005e8c7475a9ffa772e42b31f36e16
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2026-01-07 15:15:05 +0100

    [SFI-396] CSC (#1452)
    * [SFI-1379] CSC new cartridge (#1414)
    
    * feat: csc new cartridge
    
    * feat: added csc cartridge to the upload script
    
    * chore: new line
    
    * chore: new line for package json
    
    * chore: remove created cartridge
    
    * chore: show adyen as a payment option in CSC (#1418)
    
    * [SFI-1382] Handle payments on CSC (#1425)
    
    * chore: show adyen as a payment option in CSC
    
    * chore: add error as second param
    
    * chore: add live service url
    
    * chore: added N/A and pt as params
    
    * [SFI-1492] Keep order in created state (#1446)
    
    * chore: keep order in created state
    
    * chore: add aditional error return
    
    * [SFI-1434] Populate payment instrument custom props (#1450)
    
    * feat: moved webhooks logic to another cartridge (#1371)
    
    * [SFI-302] Webhook file separation (#1373)
    
    * feat: adding the new files and tests
    
    * fix: removing the constant as we can use the event code to require the handlerModule
    
    * [SFI-303] Improved the function for handling payment instruments (#1374)
    
    * feat: adding the new files and tests
    
    * fix: removing the constant as we can use the event code to require the handlerModule
    
    * feat: generic function for pi in a helper file and unit tests
    
    * chore: renaming variable name
    
    * [SFI-309] Simplification on creation of custom objects (#1376)
    
    * feat: adding the new files and tests
    
    * fix: removing the constant as we can use the event code to require the handlerModule
    
    * feat: removed refused hpp logic
    
    * fix: checking for duplicates before creating custom  objects
    
    * fix: removed complexity from handleNotify and created helper file
    
    * chore: moving hardcoded values to constants file
    
    * [SFI-303] Improved the function for handling payment instruments (#1374)
    
    * feat: adding the new files and tests
    
    * fix: removing the constant as we can use the event code to require the handlerModule
    
    * feat: generic function for pi in a helper file and unit tests
    
    * chore: renaming variable name
    
    * feat: adding the new files and tests
    
    * fix: removing the constant as we can use the event code to require the handlerModule
    
    * feat: removed refused hpp logic
    
    * chore: fixing linting issues and moving function to helper file
    
    * fix: unit tests
    
    * [SFI-1305] Move webhook logic to separate files (#1382)
    
    * feat: moved logic to specific files
    
    * chore: unit tests skeletons
    
    * chore: skipping amazon test due to instability of amazon sandbox
    
    * feat: refactoring AUTHORISATION webhook handling and unit tests
    
    * chore: linter should validate eventHandler files
    
    * [SFI-1296] Localized email (#1388)
    
    * fix: locale is passed as a parameter for localized email
    
    * chore: removed unused file and cleaned up log
    
    * chore: typo
    
    * [SFI-1352] Improvements and unit tests for webhooks (#1394)
    
    * feat: improvements and unit tests
    
    * chore: removing eslint skipping
    
    * chore: removing complexity / linting for handleCustomObject and deleteCustomObject (#1400)
    
    * Revert "chore: removing complexity / linting for handleCustomObject and deleteCustomObject (#1400)"
    
    This reverts commit ac30bb1d4ba029c10e82fa292372aa5896c9f568.
    
    * Reapply "chore: removing complexity / linting for handleCustomObject and deleteCustomObject (#1400)"
    
    This reverts commit 0e7302511a1ba24d077eb1e1c3018644e7189e43.
    
    * fix: improved and simplifies the job functions (#1413)
    
    * Case management added to the new refactored webhook cartridge (#1416)
    
    * feat: populate payment instrument custom props
    
    * chore: use collections to iterate payment instruments
    
    * chore: replicating signature cleaning as in develop
    
    ---------
    
    Co-authored-by: Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
    
    * chore: use constants from int_adyen_webhooks cartridge
    
    ---------
    
    Co-authored-by: Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
@@ -66,6 +66,7 @@ module.exports = {
 
   SERVICE: {
     PAYMENT: 'AdyenPayment',
+    PAYMENT_LINKS: 'AdyenPaymentLinks',
     PAYMENTDETAILS: 'AdyenPaymentDetails',
     RECURRING_DISABLE: 'AdyenRecurringDisable',
     POSPAYMENT: 'AdyenPosPayment',
---
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/scripts/hooks/payment/authorizeCSC.js
@@ -0,0 +1,151 @@
+const Status = require('dw/system/Status');
+const AdyenConfigs = require('*/cartridge/adyen/utils/adyenConfigs');
+const AdyenLogs = require('*/cartridge/adyen/logs/adyenCustomLogs');
+const AdyenHelper = require('*/cartridge/adyen/utils/adyenHelper');
+const constants = require('*/cartridge/adyen/config/constants');
+
+const ADYEN_PBL_NOTE_KEY = 'Adyen Payment Link';
+
+function getBillingAddressDetails(billingAddress) {
+  let billingStreet = '';
+  let billingHouseNumberOrName = '';
+  const billingCountry = billingAddress.countryCode
+    ? billingAddress.countryCode.value.toUpperCase()
+    : 'ZZ';
+
+  if (billingAddress.address1) {
+    billingStreet = billingAddress.address1;
+    if (billingAddress.address2) {
+      billingHouseNumberOrName = billingAddress.address2;
+    }
+  } else {
+    billingStreet = 'N/A';
+  }
+
+  return {
+    billingStreet,
+    billingHouseNumberOrName,
+    billingCountry,
+  };
+}
+
+function buildPaymentLinkRequest(
+  order,
+  pt,
+  billingAddress,
+  billingCountry,
+  billingStreet,
+  billingHouseNumberOrName,
+) {
+  const paymentLinkRequest = {
+    reference: order.orderNo,
+    amount: {
+      value: AdyenHelper.getCurrencyValueForApi(pt?.amount).getValueOrNull(),
+      currency: pt.amount.currencyCode,
+    },
+    countryCode: billingCountry,
+    merchantAccount: AdyenConfigs.getAdyenMerchantAccount(),
+    shopperInteraction: 'Ecommerce',
+    shopperReference: order.getCustomerNo(),
+    shopperEmail: order.getCustomerEmail(),
+    shopperLocale:
+      order.customerLocaleID === 'default' ? 'en-US' : order.customerLocaleID,
+  };
+
+  paymentLinkRequest.billingAddress = {
+    city: billingAddress.city ? billingAddress.city : 'N/A',
+    country: billingCountry,
+    houseNumberOrName: billingHouseNumberOrName,
+    postalCode: billingAddress.postalCode ? billingAddress.postalCode : 'N/A',
+    stateOrProvince: billingAddress.stateCode
+      ? billingAddress.stateCode
+      : 'N/A',
+    street: billingStreet,
+  };
+
+  return paymentLinkRequest;
+}
+
+function createAdyenPaymentLink(paymentLinkRequest) {
+  try {
+    return AdyenHelper.executeCall(
+      constants.SERVICE.PAYMENT_LINKS,
+      paymentLinkRequest,
+    );
+  } catch (e) {
+    AdyenLogs.error_log(
+      `Failed to create Adyen payment link for order ${
+        paymentLinkRequest.reference
+      }`,
+      e,
+    );
+    return null;
+  }
+}
+
+function addPaymentLinkNote(order, paymentLinkUrl) {
+  order.addNote(ADYEN_PBL_NOTE_KEY, paymentLinkUrl);
+}
+
+function authorizeAdyenPayment(order, pt) {
+  try {
+    const billingAddress = order.getBillingAddress();
+
+    const { billingStreet, billingHouseNumberOrName, billingCountry } =
+      getBillingAddressDetails(billingAddress);
+    const paymentLinkRequest = buildPaymentLinkRequest(
+      order,
+      pt,
+      billingAddress,
+      billingCountry,
+      billingStreet,
+      billingHouseNumberOrName,
+    );
+    const response = createAdyenPaymentLink(paymentLinkRequest);
+
+    if (response && response.url) {
+      addPaymentLinkNote(order, response.url);
+    }
+
+    return response.url;
+  } catch (e) {
+    AdyenLogs.error_log(
+      `Error during Adyen Payment Link authorization for order ${
+        order.orderNo
+      }`,
+      e,
+    );
+    return null;
+  }
+}
+
+function authorizeCSC(order, opi) {
+  try {
+    if (request.clientId === 'dw.csc') {
+      order.custom.Adyen_serviceChannel = 'CSC';
+      const pt = opi.getPaymentTransaction();
+      if (pt?.getPaymentProcessor().getID() === 'Adyen_Component') {
+        const paymentLink = authorizeAdyenPayment(order, pt);
+        if (paymentLink) {
+          // Intercept the flow with throwing an error.
+          // If not intercepted order will be placed and should be kept in CREATED state.
+          return new Status(
+            Status.ERROR,
+            null,
+            `Pending payment for order ${order.orderNo}. \n Payment link: ${paymentLink}`,
+          );
+        }
+        return new Status(Status.ERROR);
+      }
+    }
+  } catch (e) {
+    AdyenLogs.error_log(
+      `Failed to authorize Adyen CSC payment for order ${order.orderNo}`,
+      e,
+    );
+    return new Status(Status.ERROR);
+  }
+  return new Status(Status.OK);
+}
+
+exports.authorize = authorizeCSC;
