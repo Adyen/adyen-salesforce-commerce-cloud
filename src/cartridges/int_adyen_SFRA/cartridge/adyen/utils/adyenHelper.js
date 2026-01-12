74efe72d1534f146bd5bf1e7c256b4b38dd58644
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2026-01-12 10:47:22 +0100

    feat: tracking specific version instead ofsfra only (#1454)

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/utils/adyenHelper.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/utils/adyenHelper.js
@@ -797,7 +797,7 @@ const adyenHelperObj = {
 
     applicationInfo.externalPlatform = {
       name: constants.EXTERNAL_PLATFORM_NAME,
-      version: constants.EXTERNAL_PLATFORM_VERSION,
+      version: AdyenConfigs.getAdyenSFRAVersion(),
       integrator: AdyenConfigs.getSystemIntegratorName(),
     };
 
