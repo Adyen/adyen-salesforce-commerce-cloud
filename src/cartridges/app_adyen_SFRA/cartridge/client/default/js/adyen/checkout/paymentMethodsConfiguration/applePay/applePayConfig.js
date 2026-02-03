26440f12a51e22da45db350dd222ec3170bbc36f
Zenit Shkreli <69572953+zenit2001@users.noreply.github.com>
2026-02-03 11:15:38 +0100

    fix: resolve apple pay properly at end of checkout (#1465)

--- src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen/checkout/paymentMethodsConfiguration/applePay/applePayConfig.js
+++ src/cartridges/app_adyen_SFRA/cartridge/client/default/js/adyen/checkout/paymentMethodsConfiguration/applePay/applePayConfig.js
@@ -5,15 +5,35 @@ class ApplePayConfig {
     this.helpers = helpers;
   }
 
-  onSubmit = (state, component) => {
+  // eslint-disable-next-line class-methods-use-this
+  onAuthorized = (data, actions) => {
+    try {
+      actions.resolve();
+    } catch (error) {
+      actions.reject();
+    }
+  };
+
+  onSubmit = async (state, component, actions) => {
     $('#dwfrm_billing').trigger('submit');
-    this.helpers.paymentFromComponent(state.data, component);
+    try {
+      const response = await this.helpers.paymentFromComponent(
+        state.data,
+        component,
+      );
+      actions.resolve({
+        resultCode: response.resultCode,
+      });
+    } catch (error) {
+      actions.reject();
+    }
   };
 
   getConfig() {
     return {
       showPayButton: this.showPayButton,
       buttonColor: this.buttonColor,
+      onAuthorized: this.onAuthorized,
       onSubmit: this.onSubmit,
     };
   }
