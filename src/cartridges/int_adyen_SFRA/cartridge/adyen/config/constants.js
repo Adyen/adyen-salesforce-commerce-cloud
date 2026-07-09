a21b4cfb85e84bc5f7318365f0e5c75fe391c58f
Aleksandar Mihajlovski <aleksandar.mihajlovski@adyen.com>
2026-07-09 10:36:25 +0200

    feat: upgrade checkout component to v6.35.0 (SFI-1641)

--- src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
+++ src/cartridges/int_adyen_SFRA/cartridge/adyen/config/constants.js
@@ -168,7 +168,7 @@ module.exports = {
   APPLE_DOMAIN_URL:
     '/.well-known/apple-developer-merchantid-domain-association',
 
-  CHECKOUT_COMPONENT_VERSION: '6.30.0',
+  CHECKOUT_COMPONENT_VERSION: '6.35.0',
   CHECKOUT_API_VERSION: 'v71',
   VERSION: '26.2.0',
 };
